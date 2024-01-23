d1e13f95ba648aa2d67b735f63d121c3ade94082
snova-jamesv <150964187+snova-jamesv@users.noreply.github.com>
2024-01-23 13:58:20 -0600

    contd

--- prompt_engineering/app.py
+++ prompt_engineering/app.py
@@ -12,6 +12,8 @@ LLAMA27B_LLM_API_KEY = st.secrets["LLAMA27B_LLM_API_KEY"]
 # temp debug json on cloud
 st.write("Current directory:", os.getcwd())
 st.write("Directory contents:", os.listdir('.'))
+if STREAMLIT_SERVER_RUN_ON_SAVE:
+    st.write STREAMLIT_SERVER_RUN_ON_SAVE
 
 
 # load the json prompt example config file
