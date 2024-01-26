27aa488eb207a98e90f3e6cceb3bdaefc3c5106d
Jorge Piedrahita <jorge.piedrahita@sambanovasystems.com>
2024-01-26 15:47:37 -0500

    update docker image name

--- data_extraction/docker/start.sh
+++ data_extraction/docker/start.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 #run it from repo root dir
-#docker build -t table_ocr:v1 -f data_extraction/docker . 
+#docker build -t data_extraction:v1 -f data_extraction/docker/Dockerfile . 
 
 docker run -it \
         --rm \
         -p 8888:8888 \
         --name="data_extraction" \
-        table_ocr:v1
+        data_extraction
