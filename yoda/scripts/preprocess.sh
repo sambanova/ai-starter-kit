0e6fb4c67175258994b7226054fb619707da8a82
Imran Rashid <imran.rashid@sambanovasystems.com>
2024-02-29 13:36:01 +0000

    removed private paths

--- yoda/scripts/preprocess.sh
+++ yoda/scripts/preprocess.sh
@@ -1,15 +1,16 @@
-NUM_DP_WORKERS=8
+NUM_DP_WORKERS=
 MAX_SEQ_LEN=4096
-ROOT_GEN_DATA_PREP_DIR=/scratch/imranr/generative_data_prep/
-INPUT_FILE=/import/snvm-sc-scratch1/chenw/data/processed_data/qa_article_mix.jsonl
-OUTPUT_DIR=/scratch/imranr/YoDa/post_processed_data_7bchat/
+ROOT_GEN_DATA_PREP_DIR=
+INPUT_FILE=
+OUTPUT_DIR=
+PATH_TO_TOKENIZER=
 
 cd $ROOT_GEN_DATA_PREP_DIR && python -m generative_data_prep pipeline \
 --input_file_path=$INPUT_FILE \
 --output_path=$OUTPUT_DIR \
 --shuffle on_RAM \
 --max_seq_length=$MAX_SEQ_LEN \
---pretrained_tokenizer=/import/ml-sc-nlpcheckpoints-scratch/jonathanl/generic_checkpoints/llama_2/Llama-2-7b-chat-hf \
+--pretrained_tokenizer=$PATH_TO_TOKENIZER \
 --overwrite_output_path \
 --input_packing_config single::truncate_right \
 --num_training_splits=$NUM_DP_WORKERS \
