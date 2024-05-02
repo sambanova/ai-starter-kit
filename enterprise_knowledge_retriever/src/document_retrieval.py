0a4d24f181e5a65514ecd31d228a6ca3e06a0430
jorgep_snova <jorge.piedrahita@sambanovasystems.com>
2024-05-02 13:04:23 -0500

    minor changes

--- enterprise_knowledge_retriever/src/document_retrieval.py
+++ enterprise_knowledge_retriever/src/document_retrieval.py
@@ -261,7 +261,7 @@ class DocumentRetrieval():
             
         retriever = vectorstore.as_retriever(
             search_type="similarity_score_threshold",
-            search_kwargs={"score_threshold": self.retrieval_info["score_treshold"], "k": self.retrieval_info["k_retrieved_documents"]},
+            search_kwargs={"score_threshold": self.retrieval_info["score_threshold"], "k": self.retrieval_info["k_retrieved_documents"]},
         )
         qa_chain = RetrievalQA.from_llm(
             llm=llm,
