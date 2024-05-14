481c3e9208c6f0775dbf5b77301b947e0c0f62ed
Rodrigo Maldonado <rodrigo.maldonado@sambanovasystems.com>
2024-05-14 18:18:26 -0500

    Added metric units and configuration options in UI

--- benchmarking/streamlit/app.py
+++ benchmarking/streamlit/app.py
@@ -4,6 +4,7 @@ sys.path.append('../')
 
 import streamlit as st
 import pandas as pd
+import math
 import numpy as np
 import asyncio
 import yaml
@@ -43,8 +44,13 @@ def parse_to_df(json_data: dict) -> pd.DataFrame:
         
     return pd.DataFrame(output_dict)
         
+def get_list_parallel_workers(num_max_workers):
+    exp = int(math.log(num_max_workers, 2))
+    range = np.logspace(0, exp+1, num=exp+1, base=2, endpoint=False).tolist()
+    range = [int(number) for number in range]
+    return range
 
-def run_performance_evaluation() -> pd.DataFrame:
+def run_performance_evaluation(input_tokens, output_tokens, num_requests_worker, num_max_workers) -> pd.DataFrame:
     """Runs the performance evaluation process for different number of workers that will run in parallel.
     Each worker will run num_requests_per_worker requests.
 
@@ -54,14 +60,11 @@ def run_performance_evaluation() -> pd.DataFrame:
     
     data = []
 
-    # Number of input and outut tokens to benchmark
-    input_tokens = config['performance_eval']['input_tokens']
-    output_tokens = config['performance_eval']['output_tokens']
-    # Number of requests per worker(thread), higher gives more accurate results
-    num_requests_per_worker = config['performance_eval']['num_requests_per_worker']    
+    # Range of workers to benchmark
+    list_workers = get_list_parallel_workers(num_max_workers)
     
-    for parallel_workers in [1, 2, 4, 8]:
-        benchmark_output = asyncio.run(benchmark(parallel_workers, input_tokens, output_tokens, num_requests_per_worker))
+    for parallel_workers in list_workers:
+        benchmark_output = asyncio.run(benchmark(parallel_workers, input_tokens, output_tokens, num_requests_worker))
         data.append(benchmark_output)
         # Break if the throughput doesn't increase by more than 10%
         if len(data) > 1 and (data[-1]['performance_metrics']['throughput'] - data[-2]['performance_metrics']['throughput'])/data[-2]['performance_metrics']['throughput'] < 0.1:
@@ -84,12 +87,17 @@ def main():
         st.title("Evaluation process")
         st.markdown("**Click the button bellow to run the process**")
         
+        input_tokens = st.number_input('Number of input tokens', min_value=10, max_value=2500, value=50)
+        output_tokens = st.number_input('Number of output tokens', min_value=10, max_value=2500, value=50)
+        num_requests_worker = st.number_input('Number of requests per worker', min_value=1, max_value=20, value=5)
+        num_max_workers = st.selectbox('Maximum number of concurrent workers', options = [1,2,4,8,16], index=3)
+        
         sidebar_option = st.sidebar.button("Run!")
 
     if sidebar_option:
         with st.spinner("Processing"):
             # Process data
-            df = run_performance_evaluation()
+            df = run_performance_evaluation(input_tokens, output_tokens, num_requests_worker, num_max_workers)
 
             st.subheader("Results visualization:")
 
@@ -98,25 +106,25 @@ def main():
             col1, col2 = st.columns(2)
 
             with col1:
-                st.subheader("Latency")
+                st.subheader("Latency (s)")
                 st.pyplot(df.plot.line(x='number_workers', y='latency').figure)
             with col2:
-                st.subheader("Throughput")
+                st.subheader("Throughput (tokens/s)")
                 st.pyplot(df.plot.line(x='number_workers', y='throughput').figure)
 
             # Second row
             col3, col4 = st.columns(2)
 
             with col3:
-                st.subheader("Time To First Token")
+                st.subheader("Time To First Token (s)")
                 st.pyplot(df.plot.line(x='number_workers', y='ttft').figure)
             with col4:
-                st.subheader("Time Per Output Token")
+                st.subheader("Time Per Output Token (ms/tokem)")
                 st.pyplot(df.plot.line(x='number_workers', y='tpot').figure)
 
             # Display the table
             st.subheader("Summary table:")
-            st.write(df, hide_index=True)
+            st.write(df.set_index('number_workers'))
 
 if __name__ == "__main__":
     main()
