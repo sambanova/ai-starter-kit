08633c4000c42ab23e9ff999f175f1f22eda9b55
Rodrigo Maldonado <rodrigo.maldonado@sambanovasystems.com>
2024-06-06 12:29:02 -0500

    Changed and tested a different way to generate unlimited tokens based on model type.

--- benchmarking/src/llmperf/utils.py
+++ benchmarking/src/llmperf/utils.py
@@ -10,7 +10,8 @@ import warnings
 from transformers import LlamaTokenizerFast
 
 RESULTS_VERSION = "2023-08-31"
-NUM_RNG_ATTEMPTS = 10 # Unlikely to be used in practice: prevents eternal WHILE-loops
+NUM_RNG_ATTEMPTS = 10  # Unlikely to be used in practice: prevents eternal WHILE-loops
+
 
 class LLMPerfResults:
     """Class with LLM Performance results"""
@@ -99,65 +100,77 @@ def randomly_sample_sonnet_lines_prompt(
 
     get_token_length = lambda text: len(tokenizer.encode(text))
 
+    mistral_model_type = "mistral"
     llama3_type_name = "llama-3"
-    if llama3_type_name in model_name.lower():
-        prompt = (
-            "<|begin_of_text|><|start_header_id|>system<|end_header_id|> You are an assistant that generates prompts for LLMs. <|eot_id|>"
-            f"<|start_header_id|>user<|end_header_id|> Randomly stream lines from the following text with {expect_output_tokens} output tokens. Don't generate eot tokens:\n\nAnswer: <|eot_id|>"
-            "<|start_header_id|>assistant<|end_header_id|>"
-        )
-    else:
-        prompt = (
-            "<INST> <SYSTEM> You are an assistant that generates prompts for LLMs. </SYSTEM>\nRandomly stream lines from the following text "
-            f"with {expect_output_tokens} output tokens. "
-            "Don't generate eos tokens:\n\n </INST>"
-        )
-
-    # Get a prompt length that is at least as long as the base prompt
-    # n.b.: this means that we are not actually sampling from the Normal distribution but rather from a _censored_
-    #   Normal distribution. This will not necessarily have the requested mean and STD, but the difference is negligible 
-    #   when the mean (minus an STD or 3) is much larger than the base prompt length (i.e.: about 122 tokens for Llama-3 
-    #   model and about 57 tokens otherwise)
     num_prompt_tokens = -1
-    num_attempts = 0
-    while num_prompt_tokens < get_token_length(prompt):
+
+    if mistral_model_type in model_name.lower():
         num_prompt_tokens = sample_random_positive_int(
             prompt_tokens_mean, prompt_tokens_stddev
         )
-        num_attempts += 1
-        if num_attempts > NUM_RNG_ATTEMPTS:
-            warnings.warn(
-                f"Could not generate a long enough prompt after {NUM_RNG_ATTEMPTS} attempts. \n"
-                f"Consider increasing prompt_tokens_mean (currently: {prompt_tokens_mean}). "
-                f"Returning the default prompt instead."
+        prompt = "".join(["<s>" * (num_prompt_tokens - 1)])
+
+    elif llama3_type_name in model_name.lower():
+        num_prompt_tokens = sample_random_positive_int(
+            prompt_tokens_mean, prompt_tokens_stddev
+        )
+        prompt = "".join(["<|eot_id|>" * (num_prompt_tokens - 1)])
+
+    else:
+
+        prompt = "[INST]Your task is to repeat the lines bellow 999999 times.\n'''"
+
+        # Get a prompt length that is at least as long as the base prompt
+        # n.b.: this means that we are not actually sampling from the Normal distribution but rather from a _censored_
+        #   Normal distribution. This will not necessarily have the requested mean and STD, but the difference is negligible
+        #   when the mean (minus an STD or 3) is much larger than the base prompt length (i.e.: about 122 tokens for Llama-3
+        #   model and about 57 tokens otherwise)
+
+        num_attempts = 0
+        while num_prompt_tokens < get_token_length(prompt):
+            num_prompt_tokens = sample_random_positive_int(
+                prompt_tokens_mean, prompt_tokens_stddev
+            )
+            num_attempts += 1
+            if num_attempts > NUM_RNG_ATTEMPTS:
+                warnings.warn(
+                    f"Could not generate a long enough prompt after {NUM_RNG_ATTEMPTS} attempts. \n"
+                    f"Consider increasing prompt_tokens_mean (currently: {prompt_tokens_mean}). "
+                    f"Returning the default prompt instead."
                 )
-            num_prompt_tokens = get_token_length(prompt)
-            break
+                num_prompt_tokens = get_token_length(prompt)
+                break
 
-    sonnet_path = pathlib.Path(__file__).parent.resolve() / "sonnet.txt"
-    with open(sonnet_path, "r") as f:
-        sonnet_lines = f.readlines()
-    random.shuffle(sonnet_lines)
+        sonnet_path = pathlib.Path(__file__).parent.resolve() / "sonnet.txt"
+        with open(sonnet_path, "r") as f:
+            sonnet_lines = f.readlines()
+        random.shuffle(sonnet_lines)
 
-    # Because there isn't a 1:1 correspondence between text characters and tokens,
-    # it could be impossible to get exactly the number of input tokens requested.
-    # The following is guaranteed to return, will usually get things exactly correct,
-    # and very occasionally might generate a few tokens more than requested.
-    while num_prompt_tokens > get_token_length(prompt):
-        for line in sonnet_lines:
-            new_prompt = prompt + line
-            new_tokens = get_token_length(new_prompt)
-            if num_prompt_tokens > new_tokens:
-                prompt = new_prompt
-            elif num_prompt_tokens == new_tokens:
-                prompt = new_prompt
-                return (prompt, num_prompt_tokens)
-            else:
-                for character in line:
-                    prompt += character
-                    if num_prompt_tokens <= get_token_length(prompt):
-                        return (prompt, num_prompt_tokens)
+        # Because there isn't a 1:1 correspondence between text characters and tokens,
+        # it could be impossible to get exactly the number of input tokens requested.
+        # The following is guaranteed to return, will usually get things exactly correct,
+        # and very occasionally might generate a few tokens more than requested.
+        while num_prompt_tokens > get_token_length(prompt):
+            for line in sonnet_lines:
+                new_prompt = prompt + line
+                new_tokens = get_token_length(new_prompt)
+                if num_prompt_tokens > new_tokens:
+                    prompt = new_prompt
+                elif num_prompt_tokens == new_tokens:
+                    prompt = new_prompt
+                    # return (prompt, num_prompt_tokens)
+                    break
+                else:
+                    for character in line:
+                        prompt += character
+                        if num_prompt_tokens <= get_token_length(prompt):
+                            # return (prompt, num_prompt_tokens)
+                            break
 
+        prompt += "'''\nAnswer:[/INST]"
+
+        # print(prompt, flush=True)
+        # print(f"tokens_prompt: {get_token_length(prompt)}", flush=True)
     return (prompt, num_prompt_tokens)
 
 
@@ -182,9 +195,10 @@ def sample_random_positive_int(mean: int, stddev: int) -> int:
                 f"Could not generate a random, positive integer after {NUM_RNG_ATTEMPTS} attempts. \n"
                 f"Check your choices for mean (currently: {mean}) and stddev (currently: {stddev}). "
                 f"Returning 1 instead."
-                )
+            )
             return 1
     return ret
+
 
 def flatten_dict(d: dict, parent_key: str = "", sep: str = "_") -> dict:
     """Flattens dictionary
