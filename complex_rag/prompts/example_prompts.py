97e91e3846b2263e6a93bb8a2fa5ccaf5b9f0c6c
codym_snova <cody.macdonald@sambanovasystems.com>
2024-06-11 08:23:27 -0500

    init commi

--- complex_rag/prompts/example_prompts.py
+++ complex_rag/prompts/example_prompts.py
@@ -1,19 +1,3 @@
 examples = [
-    # General comparison
-    {"query": "query: What are the differences between device a and device b?",
-     "example": "example: What is the input voltage range, output voltage range, and switching frequency for device a and device b?"},
-    {"query": "query: What are the differences between device e and device f?",
-     "example": "example: What is the input voltage range, output voltage range, and switching frequency for device e and device f?"},
-    {"query": "query: What are the differences between device 123 and device 456?",
-     "example": "example: What is the input voltage range, output voltage range, and switching frequency for device 123 and device 456?"},
-    # Footprint compatibility
-    {"query": "query: Are device a and device b footprint compatible?",
-    "example": "example: What are the PIN package types of device a and device b?"},
-    {"query": "query: Are device c and device d footprint compatible?",
-     "example": "example: What are the PIN package types for device c and device d?"},
-    {"query": "query: Are device x and device y footprint compatible?",
-     "example": "example: What are the PIN package types of device x and device y?"},
-    # Switching frequency
-    {"query": "query: What has a higher switching frequency, device a or device b?",
-     "example": "example: What are the switching frequencies of device a and device b?"}
+
 ]
