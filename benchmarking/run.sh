c4fa0488081ab51269a500504ae004afdf519b3b
Rodrigo Maldonado <rodrigo.maldonado@sambanovasystems.com>
2024-06-14 12:08:27 -0500

    Modified some expert names and added num concurrent requests

--- benchmarking/run.sh
+++ benchmarking/run.sh
@@ -3,16 +3,17 @@
 
 # COE Turbo model
 # --model "COE/llama-2-7b-chat-hf" \
-# --model "COE/Llama-2-13B-chat-hf" \
-# --model "COE/Mistral-7B-Instruct-V0.2" \
+# --model "COE/llama-2-13b-chat-hf" \
+# --model "COE/Mistral-7B-Instruct-v0.2" \
 # --model "COE/Meta-Llama-3-8B-Instruct" \
 python src/token_benchmark_ray.py \
---model "COE/Meta-Llama-3-8B-Instruct" \
---mean-input-tokens 1000 \
+--model "COE/Mistral-7B-Instruct-v0.2" \
+--mean-input-tokens 50 \
 --stddev-input-tokens 10 \
---mean-output-tokens 1000 \
+--mean-output-tokens 50 \
 --stddev-output-tokens 10 \
---max-num-completed-requests 32 \
+--max-num-completed-requests 10 \
+--num-concurrent-requests 1 \
 --timeout 600 \
 --results-dir "./data/results/llmperf" \
 --additional-sampling-params '{}'
@@ -25,6 +26,7 @@ python src/token_benchmark_ray.py \
 # --mean-output-tokens 150 \
 # --stddev-output-tokens 10 \
 # --max-num-completed-requests 10 \
+# --num-concurrent-requests 1 \
 # --timeout 600 \
 # --results-dir "./data/results/llmperf" \
 # --additional-sampling-params '{}'
