98481530c18954d9c49b96bd5ba4c2b6184bc300
codym_snova <cody.macdonald@sambanovasystems.com>
2024-06-24 03:39:10 -0500

    minor changes

--- enterprise_knowledge_retriever/prompts/examples.py
+++ enterprise_knowledge_retriever/prompts/examples.py
@@ -1,12 +1,6 @@
 examples = [
 
-    {"query": "ADD ME",
-     "example": "ADD ME"},
-     {"query": "ADD ME",
-     "example": "ADD ME"},
-    {"query": "ADD ME",
-     "example": "ADD ME"},
-      {"query": "ADD ME",
-     "example": "ADD ME"}
+    {"query": "What are the differences between the max3000 and max2000?",
+     "example": "What are the input voltages, output voltage, and switching frequencies for the max3000 and max2000?"},
 
 ]
