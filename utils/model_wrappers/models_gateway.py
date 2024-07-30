6917c88d859895242056a3fc39b8a2e130e2a3da
jorgep_snova <jorge.piedrahita@sambanovasystems.com>
2024-07-30 11:26:28 -0500

    feat: base llm model gateway added

--- utils/model_wrappers/models_gateway.py
+++ utils/model_wrappers/models_gateway.py
@@ -1,9 +1,20 @@
+import os
+import sys
 import logging
 from typing import Optional
 from langchain_core.embeddings import Embeddings
 from langchain_core.language_models.llms import LLM
-from langchain_community.embeddings import HuggingFaceInstructEmbeddings
-from langchain_community.embeddings import SambaStudioEmbeddings
+from langchain_community.embeddings import SambaStudioEmbeddings, HuggingFaceInstructEmbeddings
+from langchain_community.llms.sambanova import SambaStudio, Sambaverse
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+utils_dir = os.path.abspath(os.path.join(current_dir, ".."))
+repo_dir = os.path.abspath(os.path.join(utils_dir, ".."))
+sys.path.append(utils_dir)
+sys.path.append(repo_dir)
+
+from utils.sambanova_endpoint import SambaStudioFastCoE
+
 
 EMBEDDING_MODEL = "intfloat/e5-large-v2"
 NORMALIZE_EMBEDDINGS = True
@@ -87,3 +98,144 @@ class ModelsGateway():
 
             return embeddings
         
+    @staticmethod
+    def load_llm(
+        type: str,
+        streaming: bool = False,
+        coe: bool = False,
+        
+        do_sample: Optional[bool] = None,
+        max_tokens_to_generate: Optional[int] = None,
+        temperature: Optional[float] = None,
+        select_expert: Optional[str] = None,
+        top_p: Optional[float] = None,
+        top_k: Optional[int] = None,
+        repetition_penalty: Optional[float] = None,
+        stop_sequences: Optional[str] = None,
+        process_prompt: Optional[bool] = False,
+        
+        sambaverse_url: Optional[str] = None,
+        sambaverse_api_key: Optional[str] = None,
+        sambaverse_model_name: Optional[str] = None,
+        
+        sambastudio_base_url: Optional[str] = None,
+        sambastudio_base_uri: Optional[str] = None,
+        sambastudio_project_id: Optional[str] = None,
+        sambastudio_endpoint_id: Optional[str] = None,
+        sambastudio_api_token: Optional[str] = None,
+        
+        fastapi_url: Optional[str] = None,
+        fastapi_api_key: Optional[str] = None,        
+        ) -> LLM:
+            """Loads a langchain Sambanova llm model given a type and parameters
+            Args:
+                type (str): wether to use sambastudio, sambaverse, or fastapi model
+                streaming (bool): wether to use streaming method. Defaults to False.
+                coe (bool): whether to use coe model. Defaults to False.
+                
+                do_sample (bool) : Optional wether to do sample.
+                max_tokens_to_generate (int) : Optional max number of tokens to generate.
+                temperature (float) : Optional model temperature.
+                select_expert (str) : Optional expert to use when using CoE models.
+                top_p (float) : Optional model top_p.
+                top_k (int) : Optional model top_k.
+                repetition_penalty (float) : Optional model repetition penalty.
+                stop_sequences (str) : Optional model stop sequences.
+                process_prompt (bool) : Optional default to false.
+                
+                sambaverse_model_name (str): Optional sambaverse model name to use.
+                sambaverse_url (str): Optional Sambaverse url to use.
+                sambaverse_api_key (str): Optional Sambaverse api key to use.
+                
+                sambastudio_base_url (str): Optional SambaStudio environment URL".
+                sambastudio_base_uri (str): Optional SambaStudio-base-URI".
+                sambastudio_project_id (str): Optional SambaStudio project ID.
+                sambastudio_endpoint_id (str): Optional SambaStudio endpoint ID.
+                sambastudio_api_token (str): Optional SambaStudio endpoint API key.
+                
+                fastapi_url (str): Optional fastApi CoE endpoint URL",
+                fastapi_api_key (str): Optional fastApi CoE endpoint API key.,
+                
+            Returns:
+                langchain llm model
+            """
+
+            if type == "sambaverse":
+                model_kwargs = {
+                    "do_sample": do_sample,
+                    "max_tokens_to_generate": max_tokens_to_generate,
+                    "temperature": temperature,
+                    "select_expert": select_expert,
+                    "top_p": top_p,
+                    "top_k": top_k,
+                    "repetition_penalty": repetition_penalty,
+                    "stop_sequences": stop_sequences,
+                    "process_prompt": process_prompt,
+                }
+                model_kwargs = {k: v for k, v in model_kwargs.items() if v is not None}
+                
+                llm = Sambaverse(
+                    sambaverse_url= sambaverse_url,
+                    sambaverse_api_key= sambaverse_api_key,
+                    sambaverse_model_name=sambaverse_model_name,
+                    model_kwargs=model_kwargs,
+                )
+            elif type == "sambastudio":
+                if coe:
+                    model_kwargs = {
+                        "do_sample": do_sample,
+                        "max_tokens_to_generate": max_tokens_to_generate,
+                        "temperature": temperature,
+                        "select_expert": select_expert,
+                        "top_p": top_p,
+                        "top_k": top_k,
+                        "repetition_penalty": repetition_penalty,
+                        "stop_sequences": stop_sequences,
+                        "process_prompt": process_prompt,
+                    }
+                    model_kwargs = {k: v for k, v in model_kwargs.items() if v is not None}
+                    
+                    llm = SambaStudio(
+                        sambastudio_base_url=sambastudio_base_url,
+                        sambastudio_base_uri=sambastudio_base_uri,
+                        sambastudio_project_id=sambastudio_project_id,
+                        sambastudio_endpoint_id=sambastudio_endpoint_id,
+                        sambastudio_api_token=sambastudio_api_token,
+                        streaming=streaming,
+                        model_kwargs=model_kwargs,
+                    )
+                else:
+                    model_kwargs = {
+                        "do_sample": do_sample,
+                        "max_tokens_to_generate": max_tokens_to_generate,
+                        "temperature": temperature,
+                        "top_p": top_p,
+                        "top_k": top_k,
+                        "repetition_penalty": repetition_penalty,
+                        "stop_sequences": stop_sequences,
+                    }
+                    model_kwargs = {k: v for k, v in model_kwargs.items() if v is not None}
+                    llm = SambaStudio(
+                        sambastudio_base_url=sambastudio_base_url,
+                        sambastudio_base_uri=sambastudio_base_uri,
+                        sambastudio_project_id=sambastudio_project_id,
+                        sambastudio_endpoint_id=sambastudio_endpoint_id,
+                        sambastudio_api_token=sambastudio_api_token,
+                        streaming=streaming,
+                        model_kwargs=model_kwargs,
+                    )
+        
+            elif type == "fastapi":
+                llm = SambaStudioFastCoE(
+                    fastapi_url = fastapi_url,
+                    fastapi_api_key = fastapi_api_key,
+                    max_tokens=max_tokens_to_generate,
+                    model=select_expert,
+                )
+            
+            else:
+                raise ValueError(
+                    f"Invalid LLM API: {type}, only 'fastapi' 'sambastudio' and 'sambaverse' are supported."
+                )
+                
+            return llm
