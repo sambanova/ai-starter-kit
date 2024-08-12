7c87197cfeb4a2924bd0993487779309697b85c2
jorgep_snova <jorge.piedrahita@sambanovasystems.com>
2024-08-12 16:36:21 -0500

    added splitting and minor improvements

--- utils/synthetic_data_gen/src/synthetic_data_gen.py
+++ utils/synthetic_data_gen/src/synthetic_data_gen.py
@@ -7,8 +7,11 @@ from dotenv import load_dotenv
 
 from typing import Dict, List, Union, Optional
 from langchain_community.llms.sambanova import SambaStudio, Sambaverse
-from langchain.prompts import BasePromptTemplate,  load_prompt
-from langchain_core.output_parsers import JsonOutputParser, CommaSeparatedListOutputParser
+from langchain_community.embeddings import SambaStudioEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.prompts import load_prompt
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_experimental.text_splitter import SemanticChunker
+from langchain_core.documents import Document
 from langchain_core.pydantic_v1 import BaseModel, Field
 
 logging.basicConfig(
@@ -44,8 +47,11 @@ class SyntheticDataGen():
         config = self.load_config(config_file) 
         self.llm_info = config["llm"]
         self.llm = self.set_llm()
+        self.embedding_model_info = config["embedding_model"]
+        self.embedding_model = self.set_embedding_model()
         self.prompts = config["prompts"]
         self.generation_config = config["generation"]
+        self.splitting_config = config["splitting"]
 
     def load_config(self, config_file) -> None:
         with open(config_file, "r") as file:
@@ -70,49 +76,91 @@ class SyntheticDataGen():
         )           
         return llm
     
+    def set_embedding_model(self) -> Union[SambaStudioEmbeddings, HuggingFaceInstructEmbeddings] :
+        embedding_model = APIGateway.load_embedding_model(
+            type=self.embedding_model_info["type"],
+            batch_size=self.embedding_model_info["batch_size"],
+            coe=self.embedding_model_info["coe"],
+            select_expert=self.embedding_model_info["select_expert"],
+        )
+        return embedding_model
+    
+    def split_documents(
+        self,
+        documents: Union[list, str],
+        breakpoint_threshold_amount: int = 95,
+        min_doc_length: Optional[int] = None,
+        ) -> List[Document]:
+        if isinstance(documents, str):
+            documents = [documents]        
+        text_splitter = SemanticChunker(
+            embeddings = self.embedding_model,
+            breakpoint_threshold_type = "percentile",
+            breakpoint_threshold_amount = breakpoint_threshold_amount,
+            sentence_split_regex = r"(?<=[.?!])\s+",
+            )
+        logging.info("splitting documents")
+        new_docs = text_splitter.create_documents(documents)
+        if min_doc_length is not None:
+            new_docs = [doc for doc in new_docs if len(doc.page_content) >= min_doc_length]
+        return new_docs
+        
     def generate_synthetic_data(
         self, 
         documents: Union[list, str],
         amount: Optional[int] = None,
+        include_context: Optional[bool] = None,
         include_thoughts: Optional[bool] = None,
         include_references: Optional[bool] = None,
-        out_file: Optional[str] = None
+        out_file: Optional[str] = None,
+        breakpoint_threshold_amount: Optional[int] = None,
+        min_doc_length: Optional[int] = None
         ) -> None:
         
         if out_file is None:
             out_file = self.generation_config["output_path"]
         if amount is None:
             amount = self.generation_config["amount_per_document"]
+        if include_context is None:
+            include_context = self.generation_config["include_context"]
         if include_thoughts is None:
             include_thoughts = self.generation_config["include_thoughts"]
         if include_references is None:
             include_references = self.generation_config["include_references"]
-            
+        if breakpoint_threshold_amount is None:
+            breakpoint_threshold_amount = self.splitting_config["breakpoint_threshold_amount"]
+        if min_doc_length is None:
+            min_doc_length = self.splitting_config["min_doc_length"]
         
         if isinstance(documents, str):
             documents = [documents]
           
-        # TODO: add semantic chunking
-            
+        documents = self.split_documents(
+            documents = documents,
+            breakpoint_threshold_amount = breakpoint_threshold_amount,
+            min_doc_length = min_doc_length
+            )
         for document in documents:
             try:
                 qa_pairs = self.generate_qa_pairs(
-                    context = document,
+                    context = document.page_content,
                     amount=amount,
+                    include_context=include_context,
                     include_thoughts=include_thoughts,
                     include_references=include_references,
                     )
                 lines=self.qa_pairs_to_prompt_completion(qa_pairs) 
                 self.update_jsonl(out_file, lines)
-                logging.info(f"Added {amount} qa pairs to {out_file}")
+                logging.info(f'Added {amount} qa pairs to {out_file}')
             except:
-                logging.warning(f"Failed to generate qa pairs for document: {document}, skipping")
+                logging.warning(f'Failed to generate qa pairs for document: "{document}", skipping')
                 
         self.remove_repeated_lines_in_place(out_file)
              
     def generate_qa_pairs(self, 
                           context: str,
                           amount: int = 5,
+                          include_context=True,
                           include_thoughts=True,
                           include_references= True
                           ): 
@@ -124,6 +172,7 @@ class SyntheticDataGen():
         for datum in generation:
             qa_pair = {
                     "question":datum["question"],
+                    "context": context if include_context else None,
                     "answer":datum["answer"],
                     "thought": datum["thought"] if include_thoughts else None,
                     "references": datum["references"] if include_references else None,
@@ -144,7 +193,7 @@ class SyntheticDataGen():
         
     def qa_pairs_to_prompt_completion(
         self,
-        qa_pairs: Union[list, dict]
+        qa_pairs: Union[List, Dict]
         ) -> list:
         if isinstance(qa_pairs, dict):
             qa_pairs = [qa_pairs]
@@ -154,6 +203,8 @@ class SyntheticDataGen():
                 "prompt":f'{self.generation_config["system_prompt"]}{pair["question"]}',
                 "completion":""
             }
+            if pair.get("context"):
+                line["prompt"] += f'\nContext: {pair["context"]}\n'            
             if pair.get("thought"):
                 line["completion"] += f'Thought: {pair["thought"]}\n'
             line["completion"] +=  f'Answer: {pair["answer"]}\n'
