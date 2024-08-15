51b540cc0d90cbb7ae75535da8aca272c1267d9d
Petro Junior Milan <petro-junior.milan@sambanovasystems.com>
2024-08-14 17:37:43 -0700

    updated Python scripts

--- search_assistant/tests/search_assistant_basic_test.py
+++ search_assistant/tests/search_assistant_basic_test.py
@@ -1,12 +1,22 @@
+#!/usr/bin/env python3
+"""
+Search Assistant (Basic Query) Test Script
+
+This script tests the functionality of the Search Assistant kit (search and answer) using unittest.
+
+Usage:
+    python tests/search_assistant_basic_test.py
+
+Returns:
+    0 if all tests pass, or a positive integer representing the number of failed tests.
+"""
+
 import os
 import sys
 
 file_dir = os.path.dirname(os.path.abspath(__file__))
 kit_dir = os.path.abspath(os.path.join(file_dir, '..'))
 repo_dir = os.path.abspath(os.path.join(kit_dir, '..'))
-
-print(kit_dir)
-print(repo_dir)
 
 sys.path.append(kit_dir)
 sys.path.append(repo_dir)
