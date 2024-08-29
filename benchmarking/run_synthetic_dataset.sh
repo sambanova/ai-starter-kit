b23686192cbaf5bbd3b876ee7474c88cf85da01b
Rodrigo Maldonado <rodrigo.maldonado@sambanovasystems.com>
2024-08-28 21:16:52 -0500

    Changes on bash process to support multiple models run

--- benchmarking/run_synthetic_dataset.sh
+++ benchmarking/run_synthetic_dataset.sh
@@ -3,36 +3,38 @@
 
 python src/evaluator.py \
 --mode synthetic \
---model-name "COE/Meta-Llama-3-8B-Instruct" \
+--model-names "llama3-8b llama3-70b	llama3-405b" \
 --results-dir "./data/results/llmperf" \
 --num-workers 1 \
 --timeout 600 \
 --num-input-tokens 1000 \
 --num-output-tokens 1000 \
 --num-requests 16 \
---llm-api "sambastudio"
+--llm-api "fastapi"
 
 # Notes:
-# 1. For CoE Models, make sure to include the prefix "COE/" before the expert name.
+# 1. For CoE Models, make sure to include the prefix "COE/" before each expert name.
 #   For example:
-#      --model-name "COE/llama-2-7b-chat-hf"
+#      --model-names "COE/llama-2-7b-chat-hf"
 #          OR
-#      --model-name "COE/llama-2-13b-chat-hf"
+#      --model-names "COE/llama-2-7b-chat-hf COE/llama-2-13b-chat-hf"
 #          OR
-#      --model-name "COE/Mistral-7B-Instruct-v0.2"
+#      --model-names "COE/llama-2-7b-chat-hf COE/Mistral-7B-Instruct-v0.2"
 #          OR
-#      --model-name "COE/Meta-Llama-3-8B-Instruct"
+#      --model-names "COE/Meta-Llama-3-8B-Instruct"
 #
 # 2. For Non-CoE models, use the model name directly and remember to update and source the `.env` file for a new endpoint.
 #   For example:
-#      --model-name "llama-2-7b-chat-hf"
+#      --model-names "llama-2-7b-chat-hf"
 #          OR
-#      --model-name "llama-2-13b-chat-hf"
+#      --model-names "llama-2-13b-chat-hf"
 #          OR
-#      --model-name "Mistral-7B-Instruct-v0.2"
+#      --model-names "Mistral-7B-Instruct-v0.2"
 #          OR
-#      --model-name "Meta-Llama-3-8B-Instruct"
+#      --model-names "Meta-Llama-3-8B-Instruct"
 #
 # 3. For FastAPI endpoints, change the llm-api parameter to "fastapi" and use the model name directly.
 #   For example:
-#      --model-name "llama3-8b"
+#      --model-names "llama3-8b"
+#          OR
+#      --model-names "llama3-8b llama3-70b"
