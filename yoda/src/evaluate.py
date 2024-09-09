ff3c2f7269f0ed2dc5806e8986a61101da7b9887
jorgep_snova <jorge.piedrahita@sambanovasystems.com>
2024-09-09 16:58:15 -0500

    removed sambaverse references

--- yoda/src/evaluate.py
+++ yoda/src/evaluate.py
@@ -2,57 +2,58 @@ import os
 import sys
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-kit_dir = os.path.abspath(os.path.join(current_dir, ".."))
-repo_dir = os.path.abspath(os.path.join(kit_dir, ".."))
+kit_dir = os.path.abspath(os.path.join(current_dir, '..'))
+repo_dir = os.path.abspath(os.path.join(kit_dir, '..'))
 
 sys.path.append(kit_dir)
 sys.path.append(repo_dir)
 
 import argparse
 import json
+
 import numpy as np
-from tqdm import tqdm
 import yaml
 from dotenv import load_dotenv
-from langchain_community.embeddings import HuggingFaceInstructEmbeddings
-from transformers import AutoTokenizer
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
+from langchain_community.embeddings import HuggingFaceInstructEmbeddings
+from tqdm import tqdm
+from transformers import AutoTokenizer
 
 from utils.model_wrappers.api_gateway import APIGateway
 from utils.vectordb.vector_db import VectorDb
-from yoda.prompts.prompts import RAG_prompt_template, LLAMA_CHAT_PROMPT_PREFIX, LLAMA_CHAT_PROMPT_POSTFIX
+from yoda.prompts.prompts import LLAMA_CHAT_PROMPT_POSTFIX, LLAMA_CHAT_PROMPT_PREFIX, RAG_prompt_template
 
-load_dotenv(os.path.join(repo_dir,'.env'))
+load_dotenv(os.path.join(repo_dir, '.env'))
 
 llm = APIGateway.load_llm(
-            type="sambastudio",
-            streaming=True,
-            coe=True,
-            do_sample=False,
-            max_tokens_to_generate=500,
-            temperature=0.0,
-            select_expert="Meta-Llama-3-8B-Instruct",
-            process_prompt=False
-        )
+    type='sambastudio',
+    streaming=True,
+    coe=True,
+    do_sample=False,
+    max_tokens_to_generate=500,
+    temperature=0.0,
+    select_expert='Meta-Llama-3-8B-Instruct',
+    process_prompt=False,
+)
 
 llm_baseline = APIGateway.load_llm(
-            type="sambaverse",
-            streaming=True,
-            coe=True,
-            do_sample=False,
-            max_tokens_to_generate=500,
-            temperature=0.0,
-            select_expert="Meta-Llama-3-8B-Instruct",
-            process_prompt=False,
-            sambaverse_model_name="Meta/Meta-Llama-3-8B-Instruct"
-        )
+    type='sambastudio',
+    streaming=True,
+    coe=True,
+    do_sample=False,
+    max_tokens_to_generate=500,
+    temperature=0.0,
+    select_expert='Meta-Llama-3-8B-Instruct',
+    process_prompt=False,
+)
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--config", default=os.path.join(kit_dir,"sn_expert_conf.yaml"),
-                        type=str, help="Path to config file")
+    parser.add_argument(
+        '--config', default=os.path.join(kit_dir, 'sn_expert_conf.yaml'), type=str, help='Path to config file'
+    )
     args = parser.parse_args()
     return args
 
@@ -66,25 +67,22 @@ if __name__ == '__main__':
     base_dir = config['src_folder']
     data_dir = config['dest_folder']
     PERSIST_DIRECTORY = os.path.join(config['vector_db_path'], 'yoda_db')
-    print(f"persist directory will be stored at: {PERSIST_DIRECTORY}")
+    print(f'persist directory will be stored at: {PERSIST_DIRECTORY}')
     # collect all context articles in one folder
 
     RAG_CONTEXT_TOP_K = config['RAG_CONTEXT_TOP_K']
 
-    RESULTS_DIR = os.path.join(kit_dir,"data","results")
+    RESULTS_DIR = os.path.join(kit_dir, 'data', 'results')
     os.makedirs(RESULTS_DIR, exist_ok=True)
-    RESULTS_FILE_PATH = os.path.join(RESULTS_DIR, "generations.json")
+    RESULTS_FILE_PATH = os.path.join(RESULTS_DIR, 'generations.json')
 
-    CUSTOMPROMPT = PromptTemplate(
-        template=RAG_prompt_template, input_variables=["context", "question"]
-    )
+    CUSTOMPROMPT = PromptTemplate(template=RAG_prompt_template, input_variables=['context', 'question'])
 
-    LLAMA_2_70B_CHAT_PATH = config["tokenizer"]
+    LLAMA_2_70B_CHAT_PATH = config['tokenizer']
     tokenizer = AutoTokenizer.from_pretrained(LLAMA_2_70B_CHAT_PATH)
 
     # read in evaluation samples
-    qa_eval_file = os.path.join(
-        data_dir, "processed_data", "article_data.jsonl")
+    qa_eval_file = os.path.join(data_dir, 'processed_data', 'article_data.jsonl')
 
     eval_data = []
     with open(qa_eval_file) as reader:
@@ -95,52 +93,52 @@ if __name__ == '__main__':
     golden_retrieved_list = []
 
     if os.path.isdir(PERSIST_DIRECTORY):
-        embedding_model = HuggingFaceInstructEmbeddings(
-            query_instruction="Represent the query for retrieval: "
+        embedding_model = HuggingFaceInstructEmbeddings(query_instruction='Represent the query for retrieval: ')
+        vectordb = VectorDb().load_vdb(
+            persist_directory=PERSIST_DIRECTORY, embedding_model=embedding_model, db_type='chroma'
         )
-        vectordb = VectorDb().load_vdb(persist_directory=PERSIST_DIRECTORY, embedding_model=embedding_model,
-                                       db_type="chroma")
     else:
-        vectordb = VectorDb().create_vdb(input_path=base_dir,
-                                         output_db=PERSIST_DIRECTORY,
-                                         chunk_size=config["chunk_size"],
-                                         chunk_overlap=config["chunk_overlap"],
-                                         db_type="faiss",
-                                         recursive=True,
-                                         tokenizer = tokenizer,
-                                         embedding_type="sambastudio",
-                                         batch_size=1,
-                                         coe=True,
-                                         select_expert="e5-mistral-7b-instruct",
-                                    )
+        vectordb = VectorDb().create_vdb(
+            input_path=base_dir,
+            output_db=PERSIST_DIRECTORY,
+            chunk_size=config['chunk_size'],
+            chunk_overlap=config['chunk_overlap'],
+            db_type='faiss',
+            recursive=True,
+            tokenizer=tokenizer,
+            embedding_type='sambastudio',
+            batch_size=1,
+            coe=True,
+            select_expert='e5-mistral-7b-instruct',
+        )
 
-    retriever = vectordb.as_retriever(search_kwargs={"k": RAG_CONTEXT_TOP_K})
+    retriever = vectordb.as_retriever(search_kwargs={'k': RAG_CONTEXT_TOP_K})
 
     qa = RetrievalQA.from_chain_type(
         llm=llm,
-        chain_type="stuff",
+        chain_type='stuff',
         retriever=retriever,
-        input_key="question",
-        output_key="response",
+        input_key='question',
+        output_key='response',
         return_source_documents=True,
     )
 
     qa.combine_documents_chain.llm_chain.prompt = CUSTOMPROMPT
 
     for example in tqdm(eval_data):
-        golden_context_filepath = example["context_filepath"]
-        question = example["question"]
+        golden_context_filepath = example['context_filepath']
+        question = example['question']
 
         # Ignorant baseline with out-of-the-box llama-7b
-        prompt = example["question"]
+        prompt = example['question']
         ignorant_pred_answer = llm_baseline(prompt=LLAMA_CHAT_PROMPT_PREFIX + prompt + LLAMA_CHAT_PROMPT_POSTFIX)
 
         # Generate answer with question and golden context
-        golden_context_prompt = example["question_with_context"]
+        golden_context_prompt = example['question_with_context']
         golden_context_answer = llm(prompt=LLAMA_CHAT_PROMPT_PREFIX + golden_context_prompt + LLAMA_CHAT_PROMPT_POSTFIX)
 
         # Generate answer with only question
-        prompt = example["question"]
+        prompt = example['question']
         pred_answer = llm(prompt=LLAMA_CHAT_PROMPT_PREFIX + prompt + LLAMA_CHAT_PROMPT_POSTFIX)
 
         # Generate answer with RAG
@@ -151,20 +149,19 @@ if __name__ == '__main__':
         golden_is_retrieved = True if golden_context_filepath in retrieved_files else False
 
         result = {
-            "question": question,
-            "ignorant_pred_answer": ignorant_pred_answer,
-            "pred_answer": pred_answer,
-            "pred_answer_with_rag": rag_answer,
-            "pred_answer_with_golden_context": golden_context_answer,
-            "ground_truth_answer": example["answer"],
-            "retrieved_rag_files": retrieved_files,
-            "golden_context_filepath": golden_context_filepath,
-            "golden_context_is_retrieved": str(golden_is_retrieved)
+            'question': question,
+            'ignorant_pred_answer': ignorant_pred_answer,
+            'pred_answer': pred_answer,
+            'pred_answer_with_rag': rag_answer,
+            'pred_answer_with_golden_context': golden_context_answer,
+            'ground_truth_answer': example['answer'],
+            'retrieved_rag_files': retrieved_files,
+            'golden_context_filepath': golden_context_filepath,
+            'golden_context_is_retrieved': str(golden_is_retrieved),
         }
         results.append(result)
         golden_retrieved_list.append(golden_is_retrieved)
 
-    results.append(
-        {"percentage of golden context being retrieved": np.mean(golden_retrieved_list)})
+    results.append({'percentage of golden context being retrieved': np.mean(golden_retrieved_list)})
     with open(RESULTS_FILE_PATH, 'w') as file:
         json.dump(results, file, indent=4)
