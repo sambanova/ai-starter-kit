2c6b1c4969d63484eb52a2b395c1aba1753fd4d2
jorgep_snova <jorge.piedrahita@sambanovasystems.com>
2024-09-09 17:24:16 -0500

    removed sambaverse wrapper and api gateway option

--- utils/model_wrappers/api_gateway.py
+++ utils/model_wrappers/api_gateway.py
@@ -4,7 +4,7 @@ import sys
 from typing import Optional
 
 from langchain_community.embeddings import HuggingFaceInstructEmbeddings  # , SambaStudioEmbeddings
-from langchain_community.llms.sambanova import SambaStudio, Sambaverse
+from langchain_community.llms.sambanova import SambaStudio
 from langchain_core.embeddings import Embeddings
 from langchain_core.language_models.llms import LLM
 
@@ -15,7 +15,7 @@ sys.path.append(utils_dir)
 sys.path.append(repo_dir)
 
 from utils.model_wrappers.langchain_embeddings import SambaStudioEmbeddings
-from utils.model_wrappers.langchain_llms import SambaNovaCloud, SambaNovaFastAPI
+from utils.model_wrappers.langchain_llms import SambaNovaCloud
 
 EMBEDDING_MODEL = 'intfloat/e5-large-v2'
 NORMALIZE_EMBEDDINGS = True
@@ -108,22 +108,17 @@ class APIGateway:
         repetition_penalty: Optional[float] = None,
         stop_sequences: Optional[str] = None,
         process_prompt: Optional[bool] = False,
-        sambaverse_url: Optional[str] = None,
-        sambaverse_api_key: Optional[str] = None,
-        sambaverse_model_name: Optional[str] = None,
         sambastudio_base_url: Optional[str] = None,
         sambastudio_base_uri: Optional[str] = None,
         sambastudio_project_id: Optional[str] = None,
         sambastudio_endpoint_id: Optional[str] = None,
         sambastudio_api_key: Optional[str] = None,
-        fastapi_url: Optional[str] = None,
-        fastapi_api_key: Optional[str] = None,
         sambanova_url: Optional[str] = None,
         sambanova_api_key: Optional[str] = None,
     ) -> LLM:
         """Loads a langchain Sambanova llm model given a type and parameters
         Args:
-            type (str): wether to use sambastudio, sambaverse, or fastapi model
+            type (str): wether to use sambastudio, or SambaNova Cloud model "sncloud"
             streaming (bool): wether to use streaming method. Defaults to False.
             coe (bool): whether to use coe model. Defaults to False.
 
@@ -137,18 +132,12 @@ class APIGateway:
             stop_sequences (str) : Optional model stop sequences.
             process_prompt (bool) : Optional default to false.
 
-            sambaverse_model_name (str): Optional sambaverse model name to use.
-            sambaverse_url (str): Optional Sambaverse url to use.
-            sambaverse_api_key (str): Optional Sambaverse api key to use.
-
             sambastudio_base_url (str): Optional SambaStudio environment URL".
             sambastudio_base_uri (str): Optional SambaStudio-base-URI".
             sambastudio_project_id (str): Optional SambaStudio project ID.
             sambastudio_endpoint_id (str): Optional SambaStudio endpoint ID.
             sambastudio_api_token (str): Optional SambaStudio endpoint API key.
 
-            fastapi_url (str): Optional fastApi CoE endpoint URL",
-            fastapi_api_key (str): Optional fastApi CoE endpoint API key.,
             sambanova_url (str): Optional SambaNova Cloud URL",
             sambanova_api_key (str): Optional SambaNovaCloud API key.
 
@@ -156,33 +145,7 @@ class APIGateway:
             langchain llm model
         """
 
-        if type == 'sambaverse':
-            envs = {
-                'sambaverse_url': sambaverse_url,
-                'sambaverse_api_key': sambaverse_api_key,
-                'sambaverse_model_name': sambaverse_model_name,
-            }
-            envs = {k: v for k, v in envs.items() if v is not None}
-
-            model_kwargs = {
-                'do_sample': do_sample,
-                'max_tokens_to_generate': max_tokens_to_generate,
-                'temperature': temperature,
-                'select_expert': select_expert,
-                'top_p': top_p,
-                'top_k': top_k,
-                'repetition_penalty': repetition_penalty,
-                'stop_sequences': stop_sequences,
-                'process_prompt': process_prompt,
-            }
-            model_kwargs = {k: v for k, v in model_kwargs.items() if v is not None}
-
-            llm = Sambaverse(
-                **envs,
-                streaming=streaming,
-                model_kwargs=model_kwargs,
-            )
-        elif type == 'sambastudio':
+        if type == 'sambastudio':
             envs = {
                 'sambastudio_base_url': sambastudio_base_url,
                 'sambastudio_base_uri': sambastudio_base_uri,
@@ -227,18 +190,6 @@ class APIGateway:
                     model_kwargs=model_kwargs,
                 )
 
-        elif type == 'fastapi':
-            envs = {
-                'fastapi_url': fastapi_url,
-                'fastapi_api_key': fastapi_api_key,
-            }
-            envs = {k: v for k, v in envs.items() if v is not None}
-            llm = SambaNovaFastAPI(
-                **envs,
-                max_tokens=max_tokens_to_generate,
-                model=select_expert,
-            )
-
         elif type == 'sncloud':
             envs = {
                 'sambanova_url': sambanova_url,
@@ -255,8 +206,6 @@ class APIGateway:
             )
 
         else:
-            raise ValueError(
-                f"Invalid LLM API: {type}, only 'sncloud', 'fastapi', 'sambastudio' and 'sambaverse' are supported."
-            )
+            raise ValueError(f"Invalid LLM API: {type}, only 'sncloud' and 'sambastudio' are supported.")
 
         return llm
