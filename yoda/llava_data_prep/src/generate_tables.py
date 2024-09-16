1319c6b12529b54001e823cdc386a74aebf54112
codym_snova <cody.macdonald@sambanovasystems.com>
2024-09-16 11:24:20 -0500

    updated generate_tables to include a note about testing and added json file loading to properly get examples for testing

--- yoda/llava_data_prep/src/generate_tables.py
+++ yoda/llava_data_prep/src/generate_tables.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+import json
 
 current_dir = os.getcwd()
 kit_dir = os.path.abspath(os.path.join(current_dir, "../.."))
@@ -16,29 +17,44 @@ DATA_DIRECTORY = os.path.join(kit_dir,"synthetic_data")
 
 table_tools = TableTools()
 
-synth_tables: dict = {}
+### Iterate using simple strings as shown below and add examples to 
+### the table_templates.json
 
-columns: str = "\\begin{tabular}{| l c c r c c r |}\n\\hline\n"
+# synth_tables: dict = {}
 
-synth_tables["Products_and_Services_Performance"] = """\\textbf{Products and Services Performance}\t\t\t\t\t\t
-\t\t\\textbf{Three Months Ended}\t\t\\textbf{Six Months Ended}\t\t
-\\hline\t\t\t\t\t\t
-****\t\\textbf{April 1,}\t\\textbf{March 26,}\t\t\\textbf{April 1,}\t\\textbf{March 26,}\t
-****\t\\textbf{2023}\t\\textbf{2022}\t\\textbf{Change}\t\\textbf{2023}\t\\textbf{2022}\t\\textbf{Change}
-\\hline\t\t\t\t\t\t
-Net sales by category\t\t\t\t\t\t
-*iPhone***\t$***51,334\t$***50,570\t****2%\t$*117,109\t$*122,198\t***(4)%
-*Mac***\t****7,168\t****10,435\t***(31)%\t**14,903\t**21,287\t**(30)%
-*iPad***\t****6,678\t****7,646\t***(13)%\t**16,066\t**14,894\t****8%
-*Wearables, Home and Accessories\t****8,757\t****8,806\t****(1)%\t**22,239\t**23,507\t**(5)%
-*Services***\t****20,907\t****19,821\t******5%\t**41,673\t**39,337\t***6%
-\\hline\t\t\t\t\t\t
-**Total new sales**\t$***94,836\t$***97,278\t*****(3)%\t$**211,990\t$**221,223\t*****(4)%
-"""
+# columns: str = "\\begin{tabular}{| l c c |}\n\\hline\n"
+
+# synth_tables["AAPL_Country_Revenues"] = """\t*****Three Months\t
+# \tDecember 31,\tDecember 25,
+# \t2022\t2021
+# Americas:*********\t\t
+# *Net sales\t$**49,278\t$**51,496
+# *Operating income\t$**17,864\t$**19,585
+# \t\t
+# Europe:*********\t\t
+# *Net sales\t$**27,681\t$**29,749
+# *Operating income\t$**10,017\t$**11,545
+# \t\t
+# Greater China:*********\t\t
+# *Net sales\t$**23,905\t$**25,783
+# *Operating income\t$**10,437\t$**11,183
+# \t\t
+# Japan:*********\t\t
+# *Net sales\t$**6,755\t$**7,107
+# *Operating income\t$**3,236\t$**3,349
+# \t\t
+# Rest of Asia Pacific:*********\t\t
+# *Net sales\t$**9,535\t$**9,810
+# *Operating income\t$**3,851\t$**3,995
+# """
+
+with open(os.path.join(kit_dir, "llava_data_prep", "table_templates/table_templates.json"), "r") as f:
+    data = json.load(f)
 
 img_name = "test_table"
-data = table_tools.convert_tsv_to_latex(columns, synth_tables["Products_and_Services_Performance"])
-path = os.path.join(DATA_DIRECTORY, "tmp")
+data = table_tools.convert_tsv_to_latex(data["aapl"]["example7"]["columns"], 
+                                        data["aapl"]["example7"]["tsv_formatted"])
+path = os.path.join(DATA_DIRECTORY, "tmp/images")
 
 table_tools.generate_images(folder_name=path,
                             image_name=img_name,
