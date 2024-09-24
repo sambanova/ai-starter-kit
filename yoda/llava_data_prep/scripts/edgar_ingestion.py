f4186816c7e4d7712d5ff5fd6d53ae9877cbd645
codym_snova <cody.macdonald@sambanovasystems.com>
2024-09-24 16:22:04 -0500

    updated edgar_ingestion.py for command line use with args

--- yoda/llava_data_prep/scripts/edgar_ingestion.py
+++ yoda/llava_data_prep/scripts/edgar_ingestion.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import argparse
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 kit_dir = os.path.abspath(os.path.join(current_dir, "../.."))
@@ -12,24 +13,46 @@ from yoda.llava_data_prep.src.edgar_ingestion import SECTools
 from yoda.llava_data_prep.src.table_utils import TableTools
 
 CONFIG_PATH = os.path.join(kit_dir, 'llava_data_prep', 'config.yaml')
-DATA_DIRECTORY = os.path.join(kit_dir, 'llava_data_prep', 'sec_data_yolo')
+DATA_DIRECTORY = os.path.join(kit_dir, 'llava_data_prep', 'sec_data')
 
-sec_tool = SECTools(config=CONFIG_PATH)
+parser = argparse.ArgumentParser(description="Download EDGAR reports, detect tables, crop tables, and store.")
 
-tickers = ["AAPL"]
-form_types = ["10-K"]
-after = "2023-01-01"
-before =  "2024-01-01"
+parser.add_argument("--name", type=str, help="Name of run", default="")
+parser.add_argument("--do-reshape", action="store_true", help="Do reshaping of table or not")
+parser.add_argument("--size", type=tuple, help="The output shape if doing reshape", default=(336,336))
+parser.add_argument("--det-model", type=str, help="model type for table detection", default="yolo",
+                    choices=["yolo", "doclaynet"])
 
-sec_tool.download_filings(tickers=tickers, 
-                          form_types=form_types, 
-                          after=after, 
-                          before=before, 
-                          download_folder=DATA_DIRECTORY)
 
-sec_tool.convert_txt_to_pdf(data_directory=DATA_DIRECTORY)
+def main() -> None:
 
-table_tools = TableTools()
+    args = parser.parse_args()
 
-table_tools.convert_pdf_to_images(data_directory=DATA_DIRECTORY)
-table_tools.crop_tables(data_directory=DATA_DIRECTORY)
+    OUTPUT_DIR = DATA_DIRECTORY + "_" + args.name
+
+    sec_tool = SECTools(config=CONFIG_PATH)
+
+    tickers = ["AAPL"]
+    form_types = ["10-K"]
+    after = "2023-01-01"
+    before =  "2024-01-01"
+
+    sec_tool.download_filings(tickers=tickers, 
+                            form_types=form_types, 
+                            after=after, 
+                            before=before, 
+                            download_folder=OUTPUT_DIR)
+
+    sec_tool.convert_txt_to_pdf(data_directory=OUTPUT_DIR)
+
+    table_tools = TableTools(do_reshape=args.do_reshape, 
+                             size=args.size)
+
+    table_tools.convert_pdf_to_images(data_directory=OUTPUT_DIR)
+    if args.det_model == "yolo":
+        table_tools.crop_tables(data_directory=OUTPUT_DIR)
+    elif args.det_model == "doclaynet":
+        table_tools.crop_tables_doclaynet(data_directory=OUTPUT_DIR)
+
+if __name__ == "__main__":
+    main()
