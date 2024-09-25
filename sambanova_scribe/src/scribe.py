98f0147d04962da337029457a5ba00f289968eb2
jorgep_snova <jorge.piedrahita@sambanovasystems.com>
2024-09-25 13:29:25 -0500

    fmt

--- sambanova_scribe/src/scribe.py
+++ sambanova_scribe/src/scribe.py
@@ -1,45 +1,45 @@
-import streamlit as st
-from typing import Callable, Dict, List, Tuple, Union
-from openai import OpenAI
 import os
 import sys
-import logging
-import yaml
-from dotenv import load_dotenv
-import yt_dlp
+from typing import Tuple, Union
+
 import streamlit as st
+import yaml
+import yt_dlp
+from dotenv import load_dotenv
 from langchain_core.language_models.chat_models import BaseChatModel
 from langchain_core.language_models.llms import LLM
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import load_prompt
-from utils.model_wrappers.api_gateway import APIGateway
+from openai import OpenAI
 
-# Set up paths
 current_dir = os.path.dirname(os.path.abspath(__file__))
-kit_dir = os.path.abspath(os.path.join(current_dir, ".."))
-repo_dir = os.path.abspath(os.path.join(kit_dir, ".."))
+kit_dir = os.path.abspath(os.path.join(current_dir, '..'))
+repo_dir = os.path.abspath(os.path.join(kit_dir, '..'))
+
+from utils.model_wrappers.api_gateway import APIGateway
 
 sys.path.append(kit_dir)
 sys.path.append(repo_dir)
-load_dotenv(os.path.join(repo_dir, ".env"))
+load_dotenv(os.path.join(repo_dir, '.env'))
 
 
-CONFIG_PATH = os.path.join(kit_dir, "config.yaml")
-MAX_FILE_SIZE=25*1024*1024 # 25 MB in bytes
+CONFIG_PATH = os.path.join(kit_dir, 'config.yaml')
+MAX_FILE_SIZE = 25 * 1024 * 1024  # 25 MB in bytes
+
 
 class FileSizeExceededError(Exception):
     pass
 
-class Scribe():
 
+class Scribe:
     def __init__(self):
         config = self.get_config_info()
-        self.llm_info=config[0]
-        self.audio_model_info=config[1]
-        self.prod_mode=config[2]
-        self.client=self.set_client()
-        self.llm=self.set_llm()
-    
+        self.llm_info = config[0]
+        self.audio_model_info = config[1]
+        self.prod_mode = config[2]
+        self.client = self.set_client()
+        self.llm = self.set_llm()
+
     def get_config_info(self) -> Tuple[str, str, str]:
         """
         Loads json config file
@@ -52,26 +52,29 @@ class Scribe():
         prod_mode = config['prod_mode']
 
         return llm_info, audio_model_info, prod_mode
-    
+
     def set_client(self):
-        
         if self.prod_mode:
             transcription_base_url = st.session_state.TRANSCRIPTION_BASE_URL
             transcription_api_key = st.session_state.TRANSCRIPTION_API_KEY
-            
+
         else:
             if 'TRANSCRIPTION_API_KEY' in st.session_state:
-                transcription_api_key = os.environ.get('TRANSCRIPTION_API_KEY') or st.session_state.TRANSCRIPTION_API_KEY
+                transcription_api_key = (
+                    os.environ.get('TRANSCRIPTION_API_KEY') or st.session_state.TRANSCRIPTION_API_KEY
+                )
             else:
                 transcription_api_key = os.environ.get('TRANSCRIPTION_API_KEY')
 
             if 'TRANSCRIPTION_BASE_URL' in st.session_state:
-                transcription_base_url  = os.environ.get('TRANSCRIPTION_BASE_URL') or st.session_state.TRANSCRIPTION_BASE_URL
+                transcription_base_url = (
+                    os.environ.get('TRANSCRIPTION_BASE_URL') or st.session_state.TRANSCRIPTION_BASE_URL
+                )
             else:
-                transcription_base_url  = os.environ.get('TRANSCRIPTION_BASE_URL')
+                transcription_base_url = os.environ.get('TRANSCRIPTION_BASE_URL')
 
         return OpenAI(base_url=transcription_base_url, api_key=transcription_api_key)
-    
+
     def set_llm(self) -> Union[LLM, BaseChatModel]:
         """
         Sets the sncloud, or sambastudio LLM based on the llm type attribute.
@@ -100,31 +103,31 @@ class Scribe():
             sambanova_api_key=sambanova_api_key,
         )
         return llm
-    
+
     def summarize(self, text, num=5):
         prompt_template = load_prompt(os.path.join(kit_dir, 'prompts', 'summary.yaml'))
-        chain = prompt_template|self.llm|StrOutputParser()
-        summary = chain.invoke({"text": text, 'num': num})
+        chain = prompt_template | self.llm | StrOutputParser()
+        summary = chain.invoke({'text': text, 'num': num})
         return summary
-    
+
     def transcribe_audio(self, audio_file):
         transcript = self.client.audio.transcriptions.create(
-            model=self.audio_model_info["model"],
+            model=self.audio_model_info['model'],
             file=audio_file,
-            language=self.audio_model_info["language"],
-            temperature=self.audio_model_info["temperature"],
+            language=self.audio_model_info['language'],
+            temperature=self.audio_model_info['temperature'],
         )
         return transcript.text
-    
-    def download_youtube_audio(self, url, output_path=None, max_filesize=MAX_FILE_SIZE):  
+
+    def download_youtube_audio(self, url, output_path=None, max_filesize=MAX_FILE_SIZE):
         if output_path is None:
-            output_path = os.path.join(kit_dir, "data")
+            output_path = os.path.join(kit_dir, 'data')
         downloaded_filename = None
 
         def progress_hook(d):
             nonlocal downloaded_filename
             if d['status'] == 'finished':
-                downloaded_filename =  d['filename']
+                downloaded_filename = d['filename']
             elif d['status'] == 'downloading':
                 if 'total_bytes' in d and d['total_bytes'] > max_filesize:
                     if 'tmpfilename' in d:
@@ -132,25 +135,27 @@ class Scribe():
                             os.remove(d['tmpfilename'])
                             print(f"Deleted temporary file: {d['tmpfilename']}")
                         except OSError as e:
-                            print(f"Error deleting temporary file: {e}")
-                    raise FileSizeExceededError(f"File size exceeds {max_filesize/1024/1024:.2f} MB limit")
+                            print(f'Error deleting temporary file: {e}')
+                    raise FileSizeExceededError(f'File size exceeds {max_filesize/1024/1024:.2f} MB limit')
 
         ydl_opts = {
             'format': 'bestaudio/best',
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192',
-            }],
+            'postprocessors': [
+                {
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '192',
+                }
+            ],
             'outtmpl': output_path + '/%(title)s.%(ext)s',
             'progress_hooks': [progress_hook],
         }
-        
+
         try:
             with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                 ydl.download([url])
-            print(f"Successfully downloaded audio from: {url}")
-            
+            print(f'Successfully downloaded audio from: {url}')
+
             # Ensure the filename has .mp3 extension
             if downloaded_filename and not downloaded_filename.endswith('.mp3'):
                 new_filename = os.path.splitext(downloaded_filename)[0] + '.mp3'
@@ -160,14 +165,13 @@ class Scribe():
             return downloaded_filename
 
         except FileSizeExceededError as e:
-            print(f"Skipped downloading {url}: {str(e)}")
+            print(f'Skipped downloading {url}: {str(e)}')
         except yt_dlp.utils.DownloadError as e:
-            print(f"An error occurred while downloading {url}: {str(e)}")
+            print(f'An error occurred while downloading {url}: {str(e)}')
         except Exception as e:
-            print(f"An unexpected error occurred while downloading {url}: {str(e)}")
-        
-        return None
+            print(f'An unexpected error occurred while downloading {url}: {str(e)}')
 
+        return None
 
     def delete_downloaded_file(self, file_path):
         try:
@@ -182,5 +186,4 @@ class Scribe():
         except OSError as e:
             print(f"OSError: Failed to delete the file '{file_path}' due to: {e.strerror}.")
         except Exception as e:
-            print(f"An unexpected error occurred: {str(e)}")
-
+            print(f'An unexpected error occurred: {str(e)}')
