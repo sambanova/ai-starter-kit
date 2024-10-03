23884e4b119dc55049d75cab47aa1380e2f55e54
Petro Junior Milan <petro-junior.milan@sambanovasystems.com>
2024-10-03 16:04:49 -0700

    changes

--- enterprise_knowledge_retriever/streamlit/app.py
+++ enterprise_knowledge_retriever/streamlit/app.py
@@ -23,6 +23,45 @@ PERSIST_DIRECTORY = os.path.join(kit_dir, f'data/my-vector-db')
 logging.basicConfig(level=logging.INFO)
 logging.info('URL: http://localhost:8501')
 
+import shutil
+from streamlit.runtime.uploaded_file_manager import UploadedFile
+from typing import List, Optional
+def save_files_user(docs: List[UploadedFile]) -> str:
+        """
+        Save all user uploaded files in Streamlit to the tmp dir with their file names
+
+        Args:
+            docs (List[UploadFile]): A list of uploaded files in Streamlit
+
+        Returns:
+            str: path where the files are saved.
+        """
+
+        # Create the data/tmp folder if it doesn't exist
+        temp_folder = os.path.join(kit_dir, 'data/tmp')
+        if not os.path.exists(temp_folder):
+            os.makedirs(temp_folder)
+        else:
+            # If there are already files there, delete them
+            for filename in os.listdir(temp_folder):
+                file_path = os.path.join(temp_folder, filename)
+                try:
+                    if os.path.isfile(file_path) or os.path.islink(file_path):
+                        os.unlink(file_path)
+                    elif os.path.isdir(file_path):
+                        shutil.rmtree(file_path)
+                except Exception as e:
+                    print(f'Failed to delete {file_path}. Reason: {e}')
+
+        # Save all selected files to the tmp dir with their file names
+        for doc in docs:
+            assert hasattr(doc, 'name'), 'doc has no attribute name.'
+            assert callable(doc.getvalue), 'doc has no method getvalue.'
+            temp_file = os.path.join(temp_folder, doc.name)
+            with open(temp_file, 'wb') as f:
+                f.write(doc.getvalue())
+
+        return temp_folder
 
 def handle_userinput(user_question: str) -> None:
     if user_question:
@@ -178,7 +217,8 @@ def main() -> None:
                 if st.button('Process'):
                     with st.spinner('Processing'):
                         # try:
-                        text_chunks = st.session_state.document_retrieval.parse_doc(docs)
+                        temp_folder = save_files_user(docs)
+                        text_chunks = st.session_state.document_retrieval.parse_doc(temp_folder)
                         if len(text_chunks) == 0:
                             st.error(
                                 """No able to get text from the documents. check your docs or try setting
@@ -203,7 +243,8 @@ def main() -> None:
                     if st.button('Process and Save database'):
                         with st.spinner('Processing'):
                             try:
-                                text_chunks = st.session_state.document_retrieval.parse_doc(docs)
+                                temp_folder = save_files_user(docs)
+                                text_chunks = st.session_state.document_retrieval.parse_doc(temp_folder)
                                 embeddings = st.session_state.document_retrieval.load_embedding_model()
                                 vectorstore = st.session_state.document_retrieval.create_vector_store(
                                     text_chunks, embeddings, output_db=save_location, collection_name=default_collection
