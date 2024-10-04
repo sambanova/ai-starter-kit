180b0e6d2288e361e9d722273a205c86a5b23681
Kwasi Ankomah <kwasi.ankomah@sambanovasystems.com>
2024-10-04 14:33:53 +0100

    updated udpdate_config.py to check for key names before update

--- update_config.py
+++ update_config.py
@@ -1,23 +1,28 @@
-# update_config.py
 import yaml
 import sys
 import os
 import re
+from typing import Dict, Any
 
-def update_config(kit_name):
+def update_config(kit_name: str) -> None:
     kit_path = os.path.join(kit_name)
     config_path = os.path.join(kit_path, 'config.yaml')
     makefile_path = os.path.join(kit_path, 'Makefile')
 
     # Update config.yaml
     with open(config_path, 'r') as file:
-        config = yaml.safe_load(file)
+        config: Dict[str, Any] = yaml.safe_load(file)
 
     # Update config as per production requirements
-    config['prod_mode'] = True
-    config['embedding_model']['type'] = 'sambastudio'
-    config['embedding_model']['batch_size'] = 32
-    config['embedding_model']['coe'] = False
+    if 'prod_mode' in config:
+        config['prod_mode'] = True
+
+    if 'embedding_model' in config:
+        embedding_model = config['embedding_model']
+        if isinstance(embedding_model, dict):
+            embedding_model['type'] = 'sambastudio'
+            embedding_model['batch_size'] = 32
+            embedding_model['coe'] = False
 
     with open(config_path, 'w') as file:
         yaml.dump(config, file)
@@ -37,13 +42,13 @@ def update_config(kit_name):
 
         with open(makefile_path, 'w') as file:
             file.write(new_makefile_content)
-        print(f"Updated STREAMLIT_PORT to 443 in {makefile_path}")
+        print(f'Updated STREAMLIT_PORT to 443 in {makefile_path}')
     else:
-        print(f"Makefile not found at {makefile_path}. Skipping STREAMLIT_PORT update.")
+        print(f'Makefile not found at {makefile_path}. Skipping STREAMLIT_PORT update.')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print("Usage: python update_config.py <kit_name>")
+        print('Usage: python update_config.py <kit_name>')
         sys.exit(1)
     kit_name = sys.argv[1]
     update_config(kit_name)
