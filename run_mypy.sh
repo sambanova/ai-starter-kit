88f3790d2c9b26ddec9b17328fe43db8459e1211
Francesca Raimondi <francesca.raimondi@sambanovasystems.com>
2024-10-07 20:21:42 +0100

    REmove install dependencies

--- run_mypy.sh
+++ run_mypy.sh
@@ -6,21 +6,8 @@ if ! command -v mypy &> /dev/null; then
     exit 1
 fi
 
-# Check if the requirements file exists
-REQUIREMENTS_FILE="./base-requirements.txt"
-if [ ! -f "$REQUIREMENTS_FILE" ]; then
-    echo "Error: The requirements file '$REQUIREMENTS_FILE' does not exist."
-    exit 1
-fi
-
-# Install dependencies from the requirements file
-if ! pip install -r "$REQUIREMENTS_FILE"; then
-    echo "Error: Failed to install dependencies."
-    exit 1
-fi
-
 # Check if the configuration file exists
-CONFIG_FILE="mypy.ini"
+CONFIG_FILE="./mypy.ini"
 if [ ! -f "$CONFIG_FILE" ]; then
     echo "Error: The configuration file '$CONFIG_FILE' does not exist."
     exit 1
