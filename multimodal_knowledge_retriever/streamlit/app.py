08529f11f8a6d67ddbb3446d14622f286ba55034
Francesca Raimondi <francesca.raimondi@sambanovasystems.com>
2024-10-08 11:35:20 +0100

    Format

--- multimodal_knowledge_retriever/streamlit/app.py
+++ multimodal_knowledge_retriever/streamlit/app.py
@@ -34,7 +34,7 @@ LVLM_MODELS = [
     'meta-llama/Llama-3.2-11B-Vision-Instruct-Turbo',
     'meta-llama/Llama-3.2-90B-Vision-Instruct-Turbo',
     'llava-v1.5-7b-4096-preview',
-    'Llama-3.2-11B-Vision-Instruct'
+    'Llama-3.2-11B-Vision-Instruct',
 ]
 # Available models in dropdown menu
 LLM_MODELS = ['Meta-Llama-3.1-70B-Instruct', 'Meta-Llama-3.1-405B-Instruct', 'Meta-Llama-3.1-8B-Instruct']
