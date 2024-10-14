a74033e59a4506e515e424498388ff51e6ec71e1
codym_snova <cody.macdonald@sambanovasystems.com>
2024-10-14 16:52:54 -0500

    updated ruff formatting for generate_fine_tune_embed_dataset.py

--- fine_tuning_embeddings/src/generate_fine_tune_embed_dataset.py
+++ fine_tuning_embeddings/src/generate_fine_tune_embed_dataset.py
@@ -12,9 +12,9 @@ from typing import Any, Dict, List, Optional, Tuple
 import yaml  # type: ignore
 from langchain_core.language_models.llms import LLM
 from llama_index.core import SimpleDirectoryReader
-from llama_index.llms.langchain import LangChainLLM
 from llama_index.core.node_parser import SimpleNodeParser
 from llama_index.core.schema import MetadataMode
+from llama_index.llms.langchain import LangChainLLM
 from tqdm import tqdm  # type: ignore
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
