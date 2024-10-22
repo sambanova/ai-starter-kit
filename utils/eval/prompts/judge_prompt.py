833a865cb2251f29223b3c42f1f3309fc39dafb4
snova-luiss <luis.salazar@sambanovasystems.com>
2024-10-22 17:06:30 -0500

    update judge prompt

--- utils/eval/prompts/judge_prompt.py
+++ utils/eval/prompts/judge_prompt.py
@@ -2,15 +2,17 @@ JUDGE_PROMPT = """
 <|begin_of_text|><|start_header_id|>system<|end_header_id|>        
 You are an evaluator who generates JSON<|eot_id|><|start_header_id|>user<|end_header_id|>"
 
-Given the evaluation steps, return a JSON with only one key: a `score` key that can take a value from 0 to 1.
+Given the evaluation steps, return a JSON with the "score" and "reason" key.
 
 Evaluation Steps:
 1. You should return a score of 0.0 if the completion contradicts the expected completion.
 2. You should return a score of 1.0 if the completion matches the expected completion.
-3. You should not reduce the score if the completion and expected completion have minor rephrasing but the content
- is correct.
+3. You should not reduce the score if the completion and expected completion have minor rephrasing or if the completion
+ larger than the expected completion but the content is correct.
 4. You should return a lower score than 1.0 if the completion and expected completion is not entirely correct,
  ignoring grammar.
+5. When providing a reason for the score, explain the merits (if any) and demerits (if any) of the completion in
+ relation to the expected completion. 
 
 query: {query}
 
@@ -25,7 +27,8 @@ IMPORTANT: Please make sure to only return in JSON format, with the "score" key 
 
 Example JSON:
 {{
-    "score": 0
+    "score": 0,
+    "reason": "The text does not follow the evaluation steps provided."
 }}
 **
 
