2951917e6e59281e4dedf7fa100c8915bfe1abc2
snova-luiss <luis.salazar@sambanovasystems.com>
2024-10-28 12:06:09 -0500

    add pydantic models

---
+++ utils/model_wrappers/tests/schemas.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class EmbeddingsBaseModel(BaseModel):
+    type: str
+    batch_size: int
+    coe: bool
+    select_expert: str
+
+
+class LLMBaseModel(BaseModel):
+    type: str
+    model: str
+    max_tokens: int
+    temperature: float
+    streaming: bool
+    coe: Optional[bool] = None
+    top_p: Optional[float] = None
+    top_k: Optional[int] = None
+    do_sample: Optional[bool] = None
+    select_expert: Optional[str] = None
