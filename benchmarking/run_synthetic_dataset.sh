9dab3b2e9bb389910d7ea5f336a9cb9307631786
snova-luiss <luis.salazar@sambanovasystems.com>
2024-11-11 12:40:45 -0500

    Update run_synthetic_dataset.sh

--- benchmarking/run_synthetic_dataset.sh
+++ benchmarking/run_synthetic_dataset.sh
@@ -13,15 +13,15 @@ python src/evaluator.py \
 --llm-api sncloud
 
 # Notes:
-# 1. For Bundle Models, make sure to include the prefix "BUNDLE/" before each expert name.
+# 1. For Bundle Models, make sure to include the prefix "Bundle/" before each expert name.
 #   For example:
-#      --model-names "BUNDLE/llama-2-7b-chat-hf"
+#      --model-names "Bundle/llama-2-7b-chat-hf"
 #          OR
-#      --model-names "BUNDLE/llama-2-7b-chat-hf BUNDLE/llama-2-13b-chat-hf"
+#      --model-names "Bundle/llama-2-7b-chat-hf Bundle/llama-2-13b-chat-hf"
 #          OR
-#      --model-names "BUNDLE/llama-2-7b-chat-hf BUNDLE/Mistral-7B-Instruct-v0.2"
+#      --model-names "Bundle/llama-2-7b-chat-hf Bundle/Mistral-7B-Instruct-v0.2"
 #          OR
-#      --model-names "BUNDLE/Meta-Llama-3-8B-Instruct"
+#      --model-names "Bundle/Meta-Llama-3-8B-Instruct"
 #
 # 2. For Non-Bundle models, use the model name directly and remember to update and source the `.env` file for a new endpoint.
 #   For example:
