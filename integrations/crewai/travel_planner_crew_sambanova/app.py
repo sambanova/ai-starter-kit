eab674aa9807ed2b32225f8db1710aafaf8b9f03
Amitabha Banerjee <amitabha.banerjee@sambanovasystems.com>
2025-01-17 11:34:58 -0800

    Fixing lint issues in Travel planner

--- integrations/crewai/travel_planner_crew_sambanova/app.py
+++ integrations/crewai/travel_planner_crew_sambanova/app.py
@@ -1,54 +1,75 @@
 #!/usr/bin/env python
-"""Travel planner based on Agentic AI workflow. Implemented using Gradio and Crew AI
-   A deployment is available at https://huggingface.co/spaces/sambanovasystems/trip-planner
 """
-import sys
+Travel planner based on Agentic AI workflow.
+
+This module deploys a portal which can customize a day to day travel itinerary
+for a person using multiple specialized AI crews.
+
+Implemented using Gradio and Crew AI
+A deployment is available at https://huggingface.co/spaces/sambanovasystems/trip-planner
+"""
+
 import json
+import logging
+
 import gradio as gr
 import plotly.graph_objects as go
-import logging
-from crew import TravelCrew, AddressSummaryCrew
+from crew import AddressSummaryCrew, TravelCrew
 
-def filter_map(text_list, lat, lon):
-    fig = go.Figure(go.Scattermapbox(
-            lat=lat,
-            lon=lon,
-            mode='markers',
-            marker=go.scattermapbox.Marker(
-                size=11
-            ),
-            hovertext=text_list
-        ))
+
+def filter_map(text_list: List[str], lat: List[str], lon: List[str]) -> go.Figure:
+    """
+    Create a Map showing the points specified in the inputs.
+
+    Args:
+        text_list: List of the description of all locations that will be shown on the map
+        lat:       List of latitude coordinates of the locations
+        lon:       List of longitude coordinates of the locations
+
+    Returns:
+        Figure: Map with the points specified in the inputs
+    """
+
+    fig = go.Figure(
+        go.Scattermapbox(lat=lat, lon=lon, mode='markers', marker=go.scattermapbox.Marker(size=11), hovertext=text_list)
+    )
 
     fig.update_layout(
-        mapbox_style="open-street-map",
+        mapbox_style='open-street-map',
         hovermode='closest',
-        mapbox=dict(
-            bearing=0,
-            center=go.layout.mapbox.Center(
-                lat=lat[1],
-                lon=lon[1]
-            ),
-            pitch=0,
-            zoom=10
-        ),
+        mapbox=dict(bearing=0, center=go.layout.mapbox.Center(lat=lat[1], lon=lon[1]), pitch=0, zoom=10),
     )
     return fig
 
-def run(origin, destination, age, trip_duration, children, budget):
-    logger.info(f"Origin: {origin}, Destination: {destination}, Age: {age}, Duration: {trip_duration}, Children: {children}, Daily Budget: {budget}")
+
+def run(origin: str, destination: str, age: int, trip_duration: int, children: bool, budget: int) -> (str, go.Figure):
+    """
+    Run the specfied query using Crew AI agents
+
+    Args:
+        origin: Origin city of the traveller
+        destination: Destination that traveller is going to
+        age: Age profile of traveller
+        children: Whether traveller has children travelling with them
+        budget: Total budget of traveller in US Dollars
+
+    Returns:
+        Returns a tuple containing the itinerary and map
+    """
+    logger.info(
+        f'Origin: {origin}, Destination: {destination}, Age: {age}, Duration: {trip_duration},'
+         ' Children: {children}, Daily Budget: {budget}'
+    )
     inputs = {
         'origin': origin,
         'destination': destination,
         'age': age,
         'trip_duration': trip_duration,
         'children': children,
-        'budget': budget
+        'budget': budget,
     }
     result = TravelCrew().crew().kickoff(inputs=inputs)
-    inputs_for_address = {
-        'text': str(result)
-    }
+    inputs_for_address = {'text': str(result)}
 
     addresses = AddressSummaryCrew().crew().kickoff(inputs=inputs_for_address)
     json_addresses = None
@@ -62,27 +83,33 @@ def run(origin, destination, age, trip_duration, children, budget):
             try:
                 json_addresses = json.loads(addresses.raw[8:-4])
             except json.JSONDecodeError as e:
-                logger.error("Error loading Crew Output for addresses")
+                logger.error('Error loading Crew Output for addresses')
                 logger.info(addresses.raw)
                 return (result, None)
-    fig = filter_map(json_addresses["name"], json_addresses["lat"], json_addresses["lon"])
+    fig = filter_map(json_addresses['name'], json_addresses['lat'], json_addresses['lon'])
     return (result, fig)
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 demo = gr.Interface(
-    title="Plan your itinerary with the help of AI",
-    description="Use this app to create a detailed itinerary on how to explore a new place. Itinerary is customized to your taste. Powered by Sambanova Cloud.",
+    title='Plan your itinerary with the help of AI',
+    description='Use this app to create a detailed itinerary on how to explore a new place.'
+                ' Itinerary is customized to your taste. Powered by Sambanova Cloud.',
     fn=run,
-    inputs=[gr.Textbox(label="Origin"), gr.Textbox(label="Destination"),
-            gr.Slider(label="Your age?", value=30, minimum=15, maximum=90, step=5),
-            gr.Slider(label="How many days are you travelling?", value=5, minimum=1, maximum=14, step=1),
-            gr.Checkbox(label="Check if children are travelling with you"),
-            gr.Slider(label="Total budget of trip in USD", show_label=True, value=1000, minimum=500, maximum=10000, step=500)],
+    inputs=[
+        gr.Textbox(label='Where are you travelling from?'),
+        gr.Textbox(label='Where are you going?'),
+        gr.Slider(label='Your age?', value=30, minimum=15, maximum=90, step=5),
+        gr.Slider(label='How many days are you travelling?', value=5, minimum=1, maximum=14, step=1),
+        gr.Checkbox(label='Check if children are travelling with you'),
+        gr.Slider(
+            label='Total budget of trip in USD', show_label=True, value=1000, minimum=500, maximum=10000, step=500
+        ),
+    ],
     outputs=[
-        gr.Textbox(label="Complete Itinerary", show_copy_button=True, autoscroll=False),
-        gr.Plot(label="Venues on a Map. Please verify with a Navigation System before traveling.")
-    ]
+        gr.Textbox(label='Complete Personalized Itinerary of your Trip', show_copy_button=True, autoscroll=False),
+        gr.Plot(label='Venues on a Map. Please verify with a Navigation System before traveling.'),
+    ],
 )
 demo.launch()
--- integrations/crewai/travel_planner_crew_sambanova/crew.py
+++ integrations/crewai/travel_planner_crew_sambanova/crew.py
@@ -1,38 +1,42 @@
-"""Implementation based on the Crew AI workflow
-"""
+"""Implementation based on the Crew AI workflow"""
 
-from crewai import Agent, Crew, Process, Task, LLM
+from crewai import LLM, Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
-
-# Uncomment the following line to use an example of a custom tool
-# from surprise_travel.tools.custom_tool import MyCustomTool
-
-from crewai_tools import SerperDevTool, ScrapeWebsiteTool
-from typing import List, Optional
-
-import os
+from crewai_tools import ScrapeWebsiteTool, SerperDevTool
 
 # Change the model which you want to use below.
-llm = LLM(model="sambanova/Meta-Llama-3.1-70B-Instruct")
+# Currently, we use the Llama 3.1 70B model because it seems the most versatile
+llm = LLM(model='sambanova/Meta-Llama-3.1-70B-Instruct')
 
 @CrewBase
-class TravelCrew():
+class TravelCrew:
     """Crew to do travel planning"""
+
     agents_config = 'config/agents.yaml'
     tasks_config = 'config/tasks.yaml'
 
     @agent
     def personalized_activity_planner(self) -> Agent:
+        """
+        An agent specialized to build an activity planner
+
+        Returns: The agent
+        """
         return Agent(
             config=self.agents_config['personalized_activity_planner'],
             llm=llm,
             max_iter=1,
-            tools=[SerperDevTool(), ScrapeWebsiteTool()], # Example of custom tool, loaded at the beginning of file
+            tools=[SerperDevTool(), ScrapeWebsiteTool()],  # Example of custom tool, loaded at the beginning of file
             allow_delegation=False,
         )
 
     @agent
     def restaurant_scout(self) -> Agent:
+        """
+        An agent specialized to scout for restaurants
+
+        Returns: The agent
+        """
         return Agent(
             config=self.agents_config['restaurant_scout'],
             llm=llm,
@@ -43,6 +47,11 @@ class TravelCrew():
 
     @agent
     def museum_scout(self) -> Agent:
+        """
+        An agent specialized to scout for museums
+
+        Returns: The agent
+        """
         return Agent(
             config=self.agents_config['museum_scout'],
             llm=llm,
@@ -53,6 +62,12 @@ class TravelCrew():
 
     @agent
     def shopping_scout(self) -> Agent:
+        """
+        An agent specialized to scout for sopping options
+
+        Returns: The agent
+        """
+
         return Agent(
             config=self.agents_config['shopping_scout'],
             llm=llm,
@@ -63,6 +78,11 @@ class TravelCrew():
 
     @agent
     def itinerary_compiler(self) -> Agent:
+        """
+        An agent specialized at composing the entire itinirary
+
+        Returns: The agent
+        """
         return Agent(
             config=self.agents_config['itinerary_compiler'],
             llm=llm,
@@ -72,68 +92,90 @@ class TravelCrew():
 
     @task
     def personalized_activity_planning_task(self) -> Task:
+        """
+        A task that designs and plans for activities.
+
+        Returns: A task
+        """
         return Task(
             config=self.tasks_config['personalized_activity_planning_task'],
             llm=llm,
             max_iter=1,
-            agent=self.personalized_activity_planner()
+            agent=self.personalized_activity_planner(),
         )
 
     @task
     def restaurant_scenic_location_scout_task(self) -> Task:
+        """
+        A task that picks restaurants.
+
+        Returns: A task
+        """
         return Task(
             config=self.tasks_config['restaurant_scenic_location_scout_task'],
             llm=llm,
             max_iter=1,
-            agent=self.restaurant_scout()
+            agent=self.restaurant_scout(),
         )
 
     @task
     def museum_scout_task(self) -> Task:
-        return Task(
-            config=self.tasks_config['museum_scout_task'],
-            llm=llm,
-            max_iter=1,
-            agent=self.museum_scout()
-        )
+        """
+        A task that plans for museums.
+
+        Returns: A task
+        """
+        return Task(config=self.tasks_config['museum_scout_task'], llm=llm, max_iter=1, agent=self.museum_scout())
 
     @task
     def shopping_scout_task(self) -> Task:
-        return Task(
-            config=self.tasks_config['shopping_scout_task'],
-            llm=llm,
-            max_iter=1,
-            agent=self.shopping_scout()
-        )
+        """
+        A task that plans for shopping.
+
+        Returns: A task
+        """
+        return Task(config=self.tasks_config['shopping_scout_task'], llm=llm, max_iter=1, agent=self.shopping_scout())
 
     @task
     def itinerary_compilation_task(self) -> Task:
+        """
+        A task that plans for museums.
+
+        Returns: A task
+        """
         return Task(
-            config=self.tasks_config['itinerary_compilation_task'],
-            llm=llm,
-            max_iter=1,
-            agent=self.itinerary_compiler()
+            config=self.tasks_config['itinerary_compilation_task'], llm=llm, max_iter=1, agent=self.itinerary_compiler()
         )
 
     @crew
     def crew(self) -> Crew:
-        """Creates the Travel crew"""
+        """
+        Creates the Travel Planning crew
+
+        Returns: A crew
+        """
         return Crew(
-            agents=self.agents, # Automatically created by the @agent decorator
-            tasks=self.tasks, # Automatically created by the @task decorator
+            agents=self.agents,
+            tasks=self.tasks,
             process=Process.sequential,
             # process=Process.hierarchical, # In case you want to use that instead https://docs.crewai.com/how-to/Hierarchical/
         )
 
 
 @CrewBase
-class AddressSummaryCrew():
+class AddressSummaryCrew:
     """Address Summary crew"""
+
     agents_config = 'config/address_agents.yaml'
     tasks_config = 'config/address_tasks.yaml'
 
     @agent
     def address_summarizer(self) -> Agent:
+        """
+        Creates an agent which can summarize addresses in a Json file
+
+        Returns: An agent
+        """
         return Agent(
             config=self.agents_config['address_summarizer'],
             llm=llm,
@@ -143,6 +185,11 @@ class AddressSummaryCrew():
 
     @task
     def address_compilation_task(self) -> Task:
+        """
+        Creates a task which can summarize addresses
+
+        Returns: A Task
+        """
         return Task(
             config=self.tasks_config['address_compilation_task'],
             llm=llm,
@@ -152,10 +199,14 @@ class AddressSummaryCrew():
 
     @crew
     def crew(self) -> Crew:
-        """Creates the AddressSummary crew"""
+        """
+        Creates the AddressSummary crew
+
+        Returns: A Crew
+        """
         crew = Crew(
-            agents=self.agents, # Automatically created by the @agent decorator
-            tasks=self.tasks, # Automatically created by the @task decorator
+            agents=self.agents,
+            tasks=self.tasks,
             process=Process.sequential,
         )
         return crew
