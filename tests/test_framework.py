6c1effd615d778a0e26a6d41b73917dde041f42e
snova-luiss <luis.salazar@sambanovasystems.com>
2025-02-10 09:13:17 -0500

    fix error

--- tests/test_framework.py
+++ tests/test_framework.py
@@ -48,8 +48,8 @@ STARTER_KITS: List[str] = [
     'multimodal_knowledge_retriever',
     'post_call_analysis',
     'prompt_engineering',
+    'document_comparison',
     'utils'
-    'document_comparison'
 ]
 
 # Dictionary to store CLI test commands for each kit
@@ -74,8 +74,8 @@ CLI_TEST_COMMANDS: Dict[str, str] = {
     'prompt_engineering': 'python tests/prompt_engineering_test.py',
     'search_assistant': 'python tests/search_assistant_test.py',
     'image_search': 'python tests/image_search_test.py',
-    'utils': 'python tests/api_testing.py',
-    'document_comparison': 'python tests/dc_test.py'
+    'document_comparison': 'python tests/dc_test.py',
+    'utils': 'python tests/api_testing.py'
 }
 
 
