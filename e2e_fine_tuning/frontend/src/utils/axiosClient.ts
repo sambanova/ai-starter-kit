ed4862c4beb45608164f7044602cd90036029fec
David Pérez <david.perez@sambanovasystems.com>
2025-03-12 18:04:06 -0500

    Add axios for fetch and api calling

---
+++ e2e_fine_tuning/frontend/src/utils/axiosClient.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+import { BASE_URL } from "@/Constants";
+
+export const axiosClient = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
