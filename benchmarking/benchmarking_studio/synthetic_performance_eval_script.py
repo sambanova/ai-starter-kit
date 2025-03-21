faa95bc28c16efa8769544ea53dbd8ab9d6c9e8f
Rodrigo Maldonado <rodrigo.maldonado@sambanovasystems.com>
2025-03-20 19:32:07 -0500

    added several changes for tracking feature and logging

--- benchmarking/benchmarking_studio/synthetic_performance_eval_script.py
+++ benchmarking/benchmarking_studio/synthetic_performance_eval_script.py
@@ -1,4 +1,6 @@
+import os
 import sys
+import yaml
 import time
 from typing import Any, Dict
 
@@ -7,91 +9,85 @@ sys.path.append('../src')
 sys.path.append('../prompts')
 sys.path.append('../src/llmperf')
 
-import pandas as pd
+# Get the absolute path of my_project
+current_dir = os.path.dirname(os.path.realpath(__file__))
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
+
+sys.path.insert(0, project_root)
 
 from benchmarking.src.llmperf import llmperf_utils
 from benchmarking.src.performance_evaluation import SyntheticPerformanceEvaluator
+from benchmarking.utils import read_json_files
 
-# This is a script to run multiple models for different benchmarking paramater combinations.
-# Here's an example of how to set the paramteres needed before running the script.
-#
-# SambaNova Cloud example:
-#
-# General parameters:
-# model_names = ['Meta-Llama-3.1-70B-Instruct', 'Meta-Llama-3.1-8B-Instruct', ...]
-# llm_api = 'sncloud'
-# results_dir = 'data/results/path/to/studio/name'
-#
-# Additional parameters:
-# timeout = 60000
-# sampling_params = {}
-# user_metadata = {}
-# time_delay = 60
-#
-# Perf parameters:
-# num_input_tokens = [100, 1_000, 10_000, 50_000, 100_000]
-# num_output_tokens = [1_000]
-# num_concurrent_requests = [1, 10, 100]
-# ratio = 5
-#
-# To run the script, go to the kit's root and run:
+from dotenv import load_dotenv
+import logging
 
-# General parameters:
-model_names = ['Meta-Llama-3.1-8B-Instruct', 'Meta-Llama-3.1-70B-Instruct']  # add more models if necessary
-llm_api = 'sncloud'  # it could be sncloud or sambastudio
-results_dir = 'data/results/path/'  # set the path where results will be saved
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    handlers=[logging.StreamHandler(sys.stdout)],
+)
+logger = logging.getLogger(__name__)
+
+load_dotenv(os.path.join(project_root,'.env'), override=True)
+
+# Read config file
+CONFIG_FILE_PATH = os.path.join(current_dir, 'config.yaml')
+with open(CONFIG_FILE_PATH) as file:
+    config = yaml.load(file, Loader=yaml.FullLoader)
+config = config['synthetic']
+config['results_dir'] = os.path.expanduser(config['results_dir'])
 
 # Additional parameters:
-timeout = 60000
+run_time = time.strftime("%Y%m%d-%H%M%S")
+results_dir = os.path.join(config['results_dir'], run_time)
 sampling_params: Dict[str, Any] = {}
 user_metadata: Dict[str, Any] = {}
-time_delay = 10  # delayed time in seconds between runs
 
-# Perf parameters:
-num_input_tokens = [1_000]  # add more input token numbers if needed
-num_output_tokens = [1_000]  # add more output token numbers if needed
-num_concurrent_requests = [1]  # add more concurrent requests token numbers if needed
-ratio = 3  # ratio between num_requests/concurrent_requests
-# e.g. means: for num_concurrent_requests = 10, there will be num_requests = 50
+for model_idx, model_name in enumerate(config['model_names']):
+    for input_tokens, output_tokens, concurrent_requests  in zip(config['input_tokens'], config['output_tokens'], config['concurrent_requests']):
+        num_requests = concurrent_requests * config['ratio']
+        
+        logging.info(
+            f'Running model_name {model_name}, input_tokens {input_tokens}, output_tokens {output_tokens},'
+            f'concurrent_requests {concurrent_requests}, num_requests {num_requests}'
+        )
+        
+        try:
+            # Instantiate evaluator
+            user_metadata['model_idx'] = model_idx
+            evaluator = SyntheticPerformanceEvaluator(
+                model_name=model_name,
+                results_dir=os.path.expanduser(results_dir),
+                num_concurrent_requests=concurrent_requests,
+                timeout=config['timeout'],
+                user_metadata=user_metadata,
+                llm_api=config['llm_api'],
+            )
 
-df_all_summary_results = pd.DataFrame()
-for model_idx, model_name in enumerate(model_names):
-    for input_tokens in num_input_tokens:
-        for output_tokens in num_output_tokens:
-            for concurrent_requests in num_concurrent_requests:
-                num_requests = concurrent_requests * ratio
-                print(
-                    f'running model_name {model_name}, input_tokens {input_tokens}, output_tokens {output_tokens},'
-                    f'concurrent_requests {concurrent_requests}, num_requests {num_requests}'
-                )
-                user_metadata['model_idx'] = model_idx
-                # Instantiate evaluator
-                evaluator = SyntheticPerformanceEvaluator(
-                    model_name=model_name,
-                    results_dir=results_dir,
-                    num_concurrent_requests=concurrent_requests,
-                    timeout=timeout,
-                    user_metadata=user_metadata,
-                    llm_api=llm_api,
-                )
+            # Run performance evaluation
+            model_results_summary, model_results_per_request = evaluator.run_benchmark(
+                num_input_tokens=input_tokens,
+                num_output_tokens=output_tokens,
+                num_requests=num_requests,
+                sampling_params=sampling_params,
+            )
+        except Exception as e:
+            logging.error(f"Error while running model_name {model_name}, input_tokens {input_tokens}, output_tokens {output_tokens}, concurrent_requests {concurrent_requests}, num_requests {num_requests}")
+            logging.error(e)
+            
+        logging.info(f"Time delay: {config['time_delay']} seconds")
+        time.sleep(config['time_delay'])
 
-                # Run performance evaluation
-                model_results_summary, model_results_per_request = evaluator.run_benchmark(
-                    num_input_tokens=input_tokens,
-                    num_output_tokens=output_tokens,
-                    num_requests=num_requests,
-                    sampling_params=sampling_params,
-                )
+if config['consolidated_results_dir']:
+    logging.info(f"Writting consolidated results to {config['consolidated_results_dir']}")
+    try:
+        df = read_json_files(results_dir, type='summary')
+        consolidated_results_dir = os.path.expanduser(config['consolidated_results_dir'])
+        consolidated_results_dir = os.path.join(consolidated_results_dir, run_time)
 
-                flatten_model_results_summary = llmperf_utils.flatten_dict(model_results_summary)
-                filtered_flatten_model_results_summary = {
-                    key: value for key, value in flatten_model_results_summary.items() if key not in ['model']
-                }
-                df_model_results_summary = pd.DataFrame.from_dict(
-                    filtered_flatten_model_results_summary,
-                    orient='index',
-                    columns=[flatten_model_results_summary['model']],
-                )
-
-                df_all_summary_results = pd.concat([df_all_summary_results, df_model_results_summary], axis=1)
-                time.sleep(time_delay)
+        os.makedirs(consolidated_results_dir, exist_ok=True)
+        df.to_excel(os.path.join(consolidated_results_dir, 'consolidated_results.xlsx'))
+    except Exception as e:
+        logging.error(f"Error while writing consolidated results to {config['consolidated_results_dir']}")
+        logging.error(e)
