6d9845d57104fbf9f3c49d8b914f3cdd4864fb27
Rodrigo Maldonado <rodrigo.maldonado@sambanovasystems.com>
2025-05-22 14:16:07 -0500

    modified get headers function to add the use debug option

--- benchmarking/src/llmperf/sambanova_client.py
+++ benchmarking/src/llmperf/sambanova_client.py
@@ -511,11 +511,16 @@ class SambaNovaCloudAPI(BaseAPIEndpoint):
 
     def _get_headers(self) -> Dict[str, str]:
         """Gets headers for API call"""
-        return {
+        
+        header = {
             'Authorization': f'Bearer {self.api_key}',
             'Content-Type': 'application/json',
-            'ss-sn-options': 'accuracy_debug',
         }
+        
+        if self.request_config.use_debugging_mode:
+            header['ss-sn-options'] = 'accuracy_debug'
+        
+        return header
 
     def _get_json_data(self) -> Dict[str, Any]:
         """Gets json body for API call
